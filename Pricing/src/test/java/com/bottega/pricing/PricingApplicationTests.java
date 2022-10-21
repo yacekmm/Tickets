182558bf3b4dff6ed7a4ee534c373650d1382439
Jacek Milewski <jacek.milewski@circlekeurope.com>
2022-10-21 10:08:42 +0200

    initial commit

---
+++ Pricing/src/test/java/com/bottega/pricing/PricingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bottega.pricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
