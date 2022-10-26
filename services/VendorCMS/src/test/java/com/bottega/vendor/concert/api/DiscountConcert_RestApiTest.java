9f7cd76e10edfc4cf5bab5611f340cd1c56cbf23
Jacek Milewski <jacek.milewski@circlekeurope.com>
2022-10-26 10:27:14 +0200

    CDC refac

--- services/VendorCMS/src/test/java/com/bottega/vendor/concert/api/DiscountConcert_RestApiTest.java
+++ services/VendorCMS/src/test/java/com/bottega/vendor/concert/api/DiscountConcert_RestApiTest.java
@@ -5,24 +5,17 @@ import com.bottega.vendor.tests.FrameworkTestBase;
 import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
-import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 
+import static com.bottega.vendor.tests.config.CdcStubs.CDC_STUB_ID_PRICING;
 import static org.apache.http.HttpStatus.SC_OK;
+import static org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties.StubsMode.LOCAL;
 
 @AutoConfigureStubRunner(
-        ids = "com.bottega:Pricing:1.0.0-SNAPSHOT:stubs:8181",
-        stubsMode = StubRunnerProperties.StubsMode.LOCAL
+        ids = CDC_STUB_ID_PRICING,
+        stubsMode = LOCAL
 )
 public class DiscountConcert_RestApiTest extends FrameworkTestBase {
 
-
-//    @ClassRule
-//    public static StubRunnerRule rule = new StubRunnerRule()
-//            .repoRoot("classpath:m2/")
-//            .downloadStub("com.bottega:Pricing:1.0.0-SNAPSHOT:stubs:8181")
-//            .stubsMode(StubRunnerProperties.StubsMode.LOCAL)
-//            .failOnNoStubs(true)
-//            ;
 
     @Test
     public void discountConcert_discounts_onValidRequest() {
