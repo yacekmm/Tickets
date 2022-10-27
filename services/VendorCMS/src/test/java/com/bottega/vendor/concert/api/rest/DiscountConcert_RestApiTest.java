58d3eae9687446cc226d69e67224ccddc27578cd
Jacek Milewski <jacek.milewski@circlekeurope.com>
2022-10-27 15:28:58 +0200

    Vendor: apiTestFix

--- services/VendorCMS/src/test/java/com/bottega/vendor/concert/api/rest/DiscountConcert_RestApiTest.java
+++ services/VendorCMS/src/test/java/com/bottega/vendor/concert/api/rest/DiscountConcert_RestApiTest.java
@@ -4,17 +4,11 @@ import com.bottega.vendor.concert.domain.Concert;
 import com.bottega.vendor.fixtures.FrameworkTestBase;
 import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.Test;
-import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 
-import static com.bottega.vendor.config.CdcStubs.CDC_STUB_ID_PRICING;
 import static org.apache.http.HttpStatus.SC_OK;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties.StubsMode.LOCAL;
 
-@AutoConfigureStubRunner(
-        ids = CDC_STUB_ID_PRICING,
-        stubsMode = LOCAL)
 public class DiscountConcert_RestApiTest extends FrameworkTestBase {
 
 
--- services/VendorCMS/src/test/java/com/bottega/vendor/fixtures/FrameworkTestBase.java
+++ services/VendorCMS/src/test/java/com/bottega/vendor/fixtures/FrameworkTestBase.java
@@ -4,12 +4,18 @@ import com.bottega.vendor.concert.fixtures.fixtures.FrameworkConcertFixtures;
 import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.test.context.ActiveProfiles;
 
+import static com.bottega.vendor.config.CdcStubs.CDC_STUB_ID_PRICING;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
+import static org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties.StubsMode.LOCAL;
 
 @SpringBootTest(webEnvironment = DEFINED_PORT)
 @ActiveProfiles({"test"})
+@AutoConfigureStubRunner(
+        ids = CDC_STUB_ID_PRICING,
+        stubsMode = LOCAL)
 public class FrameworkTestBase {
 
     @Autowired
