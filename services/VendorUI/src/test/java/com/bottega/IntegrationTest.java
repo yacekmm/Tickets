82c01e0ec9f20480e2b0fe4869e1d73546cb2008
Jacek Milewski <jacek.milewski@circlekeurope.com>
2022-10-28 13:39:06 +0200

    Initial version of VendorUI generated by generator-jhipster@7.9.3

---
+++ services/VendorUI/src/test/java/com/bottega/IntegrationTest.java
@@ -0,0 +1,20 @@
+package com.bottega;
+
+import com.bottega.VendorUiApp;
+import com.bottega.config.AsyncSyncConfiguration;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { VendorUiApp.class, AsyncSyncConfiguration.class })
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
