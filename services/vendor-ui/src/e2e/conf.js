a27779528a30e6be56b499b3aca8791bb1dcd139
Jacek Milewski <jacek.milewski@circlekeurope.com>
2022-11-14 13:18:01 +0100

    Protractor tests

--- services/vendor-ui/src/e2e/conf.js
+++ services/vendor-ui/src/e2e/conf.js
@@ -1,3 +1,13 @@
+const {SpecReporter} = require('jasmine-spec-reporter');
+
+var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
+var jasmine2HtmlReporter = new Jasmine2HtmlReporter({
+  savePath: 'reports',
+  takeScreenshots: true,
+  takeScreenshotsOnlyOnFailures: false,
+  fileName: 'protractor-report.html'
+});
+
 exports.config = {
   framework: 'jasmine',
   capabilities: {
@@ -8,4 +18,12 @@ exports.config = {
   specs: [
     './**/*.e2e-spec*'
   ],
+
+  onPrepare() {
+    require('ts-node').register({
+      project: require('path').join(__dirname, './tsconfig.e2e.json')
+    });
+    jasmine.getEnv().addReporter(new SpecReporter({spec: {displayStacktrace: true}}));
+    jasmine.getEnv().addReporter(jasmine2HtmlReporter);
+  }
 };
