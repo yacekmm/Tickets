0cd8c213a659d18989fd4ec499f541190d752003
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-11-15 13:38:20 +0100

    live-micro-assert: done

--- services/Vendor/src/test/java/com/bottega/vendor/concert/domain/ConcertDate_MicroTest.java
+++ services/Vendor/src/test/java/com/bottega/vendor/concert/domain/ConcertDate_MicroTest.java
@@ -1,76 +1,47 @@
 package com.bottega.vendor.concert.domain;
 
-import com.bottega.sharedlib.vo.error.ErrorResult;
-import com.bottega.vendor.concert.fixtures.ConcertLogicTestBase;
-import io.vavr.control.Validation;
+import com.bottega.vendor.concert.fixtures.*;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.*;
-
-import java.util.stream.Stream;
 
 import static com.bottega.sharedlib.config.TestClockConfig.TEST_TIME;
-import static com.bottega.sharedlib.vo.error.ErrorType.BAD_REQUEST;
-import static com.bottega.vendor.concert.api.app.ConcertErrorCode.invalid_date;
 import static com.bottega.vendor.concert.domain.ConcertDate.from;
-import static com.bottega.vendor.concert.fixtures.ConcertDateAssert.assertThatConcertDate;
-import static java.time.LocalDate.of;
 import static java.time.temporal.ChronoUnit.DAYS;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class ConcertDate_MicroTest extends ConcertLogicTestBase {
-
-    public static Stream<Arguments> tooEarlyDatesSource() {
-        return Stream.of(
-                arguments(TEST_TIME.minus(1, DAYS).toString()),
-                arguments(TEST_TIME.toString()),
-                arguments(TEST_TIME.plus(6, DAYS).toString()),
-                arguments(TEST_TIME.plus(7, DAYS).toString())
-        );
-    }
 
     @Test
     public void fromString_OK_onDateString(){
         //expect
-        assertThatConcertDate(from("2022-02-22", sharedFixtures.clock)).isEqualTo(2022, 2, 22);
-        assertThatConcertDate(from("2025-01-01", sharedFixtures.clock)).isEqualTo(2025, 1, 1);
-        assertThatConcertDate(from("2025-12-31", sharedFixtures.clock)).isEqualTo(2025, 12, 31);
+        ConcertDateAssert.assertThatConcertDate(from("2022-02-22", sharedFixtures.clock)).isEqualTo(2022, 2, 22);
+        ConcertDateAssert.assertThatConcertDate(from("2025-01-01", sharedFixtures.clock)).isEqualTo(2025, 1, 1);
+        ConcertDateAssert.assertThatConcertDate(from("2025-12-31", sharedFixtures.clock)).isEqualTo(2025, 12, 31);
     }
 
     @Test
     public void fromString_OK_onDateTimeString(){
         //expect
-        assertThatConcertDate(from("2022-02-27T07:20:00Z", sharedFixtures.clock)).isEqualTo(2022, 2, 27);
-    }
-
-
-    @Test
-    public void fromString_returnsError_onInvalidString(){
-        //when
-        Validation<ErrorResult, ConcertDate> result = ConcertDate.from("invalid", sharedFixtures.clock);
-
-        //then
-        assertThat(result.getError().getType()).isEqualTo(BAD_REQUEST);
-        assertThat(result.getError().getCode()).isEqualTo(invalid_date);
-        assertThat(result.getError().getDescription()).startsWith("Unsupported date format:");
-    }
-
-    @ParameterizedTest
-    @MethodSource("tooEarlyDatesSource")
-    public void fromString_returnsError_onDateUnderMinimumThreshold(String invalidDate){
-        //when
-        Validation<ErrorResult, ConcertDate> result = ConcertDate.from(invalidDate, sharedFixtures.clock);
-
-        //then
-        assertThat(result.getError().getType()).isEqualTo(BAD_REQUEST);
-        assertThat(result.getError().getCode()).isEqualTo(invalid_date);
-        assertThat(result.getError().getDescription()).startsWith("Too early");
+        ConcertDateAssert.assertThatConcertDate(from("2022-02-27T07:20:00Z", sharedFixtures.clock)).isEqualTo(2022, 2, 27);
     }
 
     @Test
-    public void fromString_OK_onDateRightOverThreshold(){
+    public void fromString_badRequest_onInvalidString(){
         //expect
-        assertThat(ConcertDate.from(TEST_TIME.plus(8, DAYS).toString(), sharedFixtures.clock).get().getUtcDate()).isEqualTo(of(2022, 2, 13));
+        ConcertDateAssert.assertThatConcertDate(from("invalid", sharedFixtures.clock)).hasInvalidDateError("Unsupported date format: Text 'invalid' could not be parsed at index 0");
+    }
+
+    @Test
+    public void fromString_error_onPastDate(){
+        //expect
+        ConcertDateAssert.assertThatConcertDate(from(TEST_TIME.minus(1, DAYS).toString(), sharedFixtures.clock)).hasInvalidDateError("Too early");
+        ConcertDateAssert.assertThatConcertDate(from(TEST_TIME.minus(30, DAYS).toString(), sharedFixtures.clock)).hasInvalidDateError("Too early");
+    }
+
+    @Test
+    public void fromString_error_onDateUnderMinimumThreshold(){
+        //expect
+        ConcertDateAssert.assertThatConcertDate(from(TEST_TIME.plus(6, DAYS).toString(), sharedFixtures.clock)).hasInvalidDateError("Too early");
+        ConcertDateAssert.assertThatConcertDate(from(TEST_TIME.plus(7, DAYS).toString(), sharedFixtures.clock)).hasInvalidDateError("Too early");
+        ConcertDateAssert.assertThatConcertDate(from(TEST_TIME.plus(8, DAYS).toString(), sharedFixtures.clock)).isValid();
+        ConcertDateAssert.assertThatConcertDate(from(TEST_TIME.plus(9, DAYS).toString(), sharedFixtures.clock)).isValid();
     }
 }
--- services/Vendor/src/test/java/com/bottega/vendor/concert/fixtures/ConcertDateAssert.java
+++ services/Vendor/src/test/java/com/bottega/vendor/concert/fixtures/ConcertDateAssert.java
@@ -6,6 +6,8 @@ import io.vavr.control.Validation;
 import lombok.RequiredArgsConstructor;
 import org.assertj.core.api.Assertions;
 
+import static com.bottega.sharedlib.vo.error.ErrorType.BAD_REQUEST;
+import static com.bottega.vendor.concert.api.app.ConcertErrorCode.invalid_date;
 import static java.time.LocalDate.of;
 import static org.assertj.vavr.api.VavrAssertions.assertThat;
 
@@ -19,6 +21,19 @@ public class ConcertDateAssert {
 
         assertThat(concertDate).isValid();
         Assertions.assertThat(concertDate.get().getUtcDate()).isEqualTo(of(year, month, day));
+        return this;
+    }
+
+    public ConcertDateAssert hasInvalidDateError(String description) {
+        assertThat(concertDate).isInvalid();
+        Assertions.assertThat(concertDate.getError().getType()).isEqualTo(BAD_REQUEST);
+        Assertions.assertThat(concertDate.getError().getCode()).isEqualTo(invalid_date);
+        Assertions.assertThat(concertDate.getError().getDescription()).isEqualTo(description);
+        return this;
+    }
+
+    public ConcertDateAssert isValid() {
+        assertThat(concertDate).isValid();
         return this;
     }
 }
