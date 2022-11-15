1d3a92eb14233e7668b81b77a1498c7e1fcdf390
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-11-15 23:11:24 +0100

    live-comp-repo: not found error, success test implementation started

--- services/Pricing/src/test/java/com/bottega/pricing/price/api/app/ApplyPercentageFactor_CompTest.java
+++ services/Pricing/src/test/java/com/bottega/pricing/price/api/app/ApplyPercentageFactor_CompTest.java
@@ -1,17 +1,26 @@
 package com.bottega.pricing.price.api.app;
 
 import com.bottega.pricing.fixtures.LogicTestBase;
+import com.bottega.pricing.price.domain.ItemPrice;
+import com.bottega.sharedlib.fixtures.ErrorAssert;
+import com.bottega.sharedlib.vo.error.ErrorResult;
+import io.vavr.control.Either;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.vavr.api.VavrAssertions.assertThat;
 
 class ApplyPercentageFactor_CompTest extends LogicTestBase {
 
     @Test
     void applyPercentageFactor_returnsSingleDiscountedPrice_onValidRequest() {
         //given
-
+        ItemPrice price = builders.aPrice().build();
+        priceFixtures.itemPriceRepo.save(price);
 
         //when
-
+        Either<ErrorResult, List<ItemPrice>> result = priceFixtures.priceService.applyPercentageFactor("item-id", 10);
 
         //then
     }
@@ -19,9 +28,11 @@ class ApplyPercentageFactor_CompTest extends LogicTestBase {
     @Test
     void applyPercentageFactor_returnsError_onNoPriceFound() {
         //when
-
+        Either<ErrorResult, List<ItemPrice>> result = priceFixtures.priceService.applyPercentageFactor("item-id", 10);
 
         //then
-
+        assertThat(result).hasLeftValueSatisfying(errorResult ->
+                ErrorAssert.assertThatError(errorResult)
+                        .isNotFound("No price entries found for requested item. itemId: item-id"));
     }
 }
