ed47f60fc4efd0c582fa05af220b9854bc68c5e0
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-11-15 23:57:53 +0100

    live-comp-comp: done

--- services/Pricing/src/main/java/com/bottega/pricing/price/api/app/PriceService.java
+++ services/Pricing/src/main/java/com/bottega/pricing/price/api/app/PriceService.java
@@ -33,7 +33,7 @@ public class PriceService {
         List<ItemPrice> updatedPrices = priceRepo.findByItemId(itemId).stream()
                 .map(itemPrice -> itemPrice.applyFactor(percentageFactor(percentage, itemPrice)))
                 .peek(priceRepo::save)
-                //TODO update read model via priceUpdateService
+                .peek(priceUpdateService::updateReadModel)
                 //TODO publish price_changed event
                 .toList();
 
