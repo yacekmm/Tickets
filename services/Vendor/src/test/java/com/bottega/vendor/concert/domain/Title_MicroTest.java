bc8c06ca28219309660bb8a9c4262b9a863e2017
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-11-15 13:26:13 +0100

    task-micro: sample on happy path

--- services/Vendor/src/test/java/com/bottega/vendor/concert/domain/Title_MicroTest.java
+++ services/Vendor/src/test/java/com/bottega/vendor/concert/domain/Title_MicroTest.java
@@ -3,11 +3,14 @@ package com.bottega.vendor.concert.domain;
 import com.bottega.vendor.concert.fixtures.ConcertLogicTestBase;
 import org.junit.jupiter.api.Test;
 
+import static com.bottega.vendor.concert.domain.Title.from;
+import static org.assertj.core.api.Assertions.assertThat;
+
 class Title_MicroTest extends ConcertLogicTestBase {
 
     @Test
     public void fromString_OK_onValidString(){
         //expect
+        assertThat(from("This is a valid title").get().getValue()).isEqualTo("This is a valid title");
     }
-
 }
