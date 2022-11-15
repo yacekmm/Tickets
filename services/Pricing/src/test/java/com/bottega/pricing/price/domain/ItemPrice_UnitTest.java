7cb4dcc33d54a8d7f59d18c016c0eda4db7a13f6
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-11-15 21:10:44 +0100

    live-unit-builder: constructor, withPrice in ItemPriceBuilder

--- services/Pricing/src/test/java/com/bottega/pricing/price/domain/ItemPrice_UnitTest.java
+++ services/Pricing/src/test/java/com/bottega/pricing/price/domain/ItemPrice_UnitTest.java
@@ -1,20 +1,24 @@
 package com.bottega.pricing.price.domain;
 
 import com.bottega.pricing.fixtures.LogicTestBase;
-import com.bottega.pricing.price.fixtures.PriceAssert;
-import com.bottega.sharedlib.fixtures.UUIDs;
-import com.bottega.sharedlib.repo.MoneyDbEntity;
-import com.bottega.sharedlib.vo.Money;
-import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
+import com.bottega.pricing.price.fixtures.*;
+import org.junit.jupiter.api.*;
 
 class ItemPrice_UnitTest extends LogicTestBase {
+
+    private ItemPriceBuilder itemPriceBuilder;
+
+    @BeforeEach
+    void setUp() {
+        itemPriceBuilder = new ItemPriceBuilder();
+    }
 
     @Test
     void applyPercentageFactor_discountsPrice_onValidRequest() {
         //given
-        ItemPrice itemPrice = new ItemPrice(new PriceId(), MoneyDbEntity.from(new Money(200_00)), UUIDs.zeros(), new ArrayList<>());
+        ItemPrice itemPrice = itemPriceBuilder
+                .withPrice(200_00)
+                .build();
 
         //when
         ItemPrice result = itemPrice.applyFactor(PriceFactorFactory.percentageFactor(10, itemPrice));
--- services/Pricing/src/test/java/com/bottega/pricing/price/fixtures/ItemPriceBuilder.java
+++ services/Pricing/src/test/java/com/bottega/pricing/price/fixtures/ItemPriceBuilder.java
@@ -1,10 +1,27 @@
 package com.bottega.pricing.price.fixtures;
 
+import com.bottega.pricing.price.domain.*;
+import com.bottega.sharedlib.fixtures.UUIDs;
+import com.bottega.sharedlib.repo.MoneyDbEntity;
+import com.bottega.sharedlib.vo.Money;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
 
 @Component
 @RequiredArgsConstructor
 public class ItemPriceBuilder {
 
+
+    private int price = 200_00;
+
+    public ItemPrice build() {
+        return new ItemPrice(new PriceId(), MoneyDbEntity.from(new Money(price)), UUIDs.zeros(), new ArrayList<>());
+    }
+
+    public ItemPriceBuilder withPrice(int price) {
+        this.price = price;
+        return this;
+    }
 }
