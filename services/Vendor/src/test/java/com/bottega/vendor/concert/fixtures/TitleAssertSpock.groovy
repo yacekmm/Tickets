574c599853e8987d527bcdb3bad38b83729e8d0b
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-11-15 13:57:43 +0100

    task-micro-spock: start

--- services/Vendor/src/test/java/com/bottega/vendor/concert/fixtures/TitleAssertSpock.groovy
+++ services/Vendor/src/test/java/com/bottega/vendor/concert/fixtures/TitleAssertSpock.groovy
@@ -6,9 +6,6 @@ import groovy.transform.TupleConstructor
 import io.vavr.control.Validation
 import spock.lang.Specification
 
-import static com.bottega.sharedlib.vo.error.ErrorType.BAD_REQUEST
-import static com.bottega.vendor.concert.api.app.ConcertErrorCode.invalid_title
-
 @TupleConstructor
 class TitleAssertSpock extends Specification {
 
@@ -18,27 +15,4 @@ class TitleAssertSpock extends Specification {
         new TitleAssertSpock(actual)
     }
 
-    TitleAssertSpock isEqualTo(String expectedTitle) {
-        assert actualTitle.isValid()
-        assert actualTitle.get().value == expectedTitle
-        this
-    }
-
-    TitleAssertSpock hasInvalidLengthError() {
-        hasInvalidTitleError("Title length must be between 10 and 160 chars")
-    }
-
-    TitleAssertSpock hasBannedWordError(String expectedBannedWord) {
-        hasInvalidTitleError("Title must not contain banned word: " + expectedBannedWord)
-    }
-
-    protected TitleAssertSpock hasInvalidTitleError(String expectedErrorDesc) {
-        assert actualTitle.isInvalid()
-        with(actualTitle.getError()) {
-            type == BAD_REQUEST
-            code == invalid_title
-            description == expectedErrorDesc
-        }
-        this
-    }
 }
