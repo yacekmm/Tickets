f8d7131f26aa4ee5fa17252ac2e03878b4be4496
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-11-15 13:54:51 +0100

    live-micro-spock: sample

--- services/Vendor/src/test/java/com/bottega/vendor/concert/domain/ConcertDate_MicroSpec.groovy
+++ services/Vendor/src/test/java/com/bottega/vendor/concert/domain/ConcertDate_MicroSpec.groovy
@@ -2,7 +2,28 @@ package com.bottega.vendor.concert.domain
 
 import com.bottega.vendor.fixtures.SpecificationBase
 
+import static com.bottega.sharedlib.config.TestClockConfig.TEST_TIME
+import static com.bottega.vendor.concert.domain.ConcertDate.from
+import static com.bottega.vendor.concert.fixtures.ConcertDateAssertSpock.assertThatConcertDate
+import static java.time.LocalDate.of
+import static java.time.temporal.ChronoUnit.DAYS
+
 class ConcertDate_MicroSpec extends SpecificationBase {
 
+    def "fromString - OK - on valid input"() {
 
+        expect:
+        assertThatConcertDate(from(dateString, sharedFixtures.clock))
+                .isEqualTo(expectedDate)
+
+        where:
+        dateString                         | expectedDate
+        "2022-02-22"                       | of(2022, 2, 22)
+        "2025-01-01"                       | of(2025, 1, 1)
+        "2025-12-31"                       | of(2025, 12, 31)
+        "2022-02-27T07:20:00Z"             | of(2022, 2, 27)
+
+        //closest acceptable date after minDate threshold
+        TEST_TIME.plus(8, DAYS).toString() | of(2022, 2, 13)
+    }
 }
