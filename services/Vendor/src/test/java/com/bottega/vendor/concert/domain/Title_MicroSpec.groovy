06455984b759ed0d0aecf3543265a34a8fb26bb3
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-11-15 13:59:27 +0100

    task-micro-spock: done

--- services/Vendor/src/test/java/com/bottega/vendor/concert/domain/Title_MicroSpec.groovy
+++ services/Vendor/src/test/java/com/bottega/vendor/concert/domain/Title_MicroSpec.groovy
@@ -23,4 +23,34 @@ class Title_MicroSpec extends SpecificationBase {
         "a".multiply(160)            | "a".multiply(160)
         "a".multiply(160) + " "      | "a".multiply(160)
     }
+
+    def "fromString - returns error - on invalid length"() {
+
+        expect:
+        assertThatTitle(from(titleString))
+                .hasInvalidLengthError()
+
+        where:
+        titleString << [
+                null,
+                "a" * 9,
+                "a" * 161,
+                "a" * 9 + " "]
+    }
+
+    def "fromString - returns error - on banned words in title"() {
+
+        expect:
+        assertThatTitle(from(titleString))
+                .hasBannedWordError(expectedBannedWord)
+
+        where:
+        titleString                | expectedBannedWord
+        "rage against the machine" | "rage"
+        "VIOLENCE"                 | "violence"
+        "   Snickers   "           | "snickers"
+        "rage against the machine" | "rage"
+        "machine against the rage" | "rage"
+        "machine rage machine"     | "rage"
+    }
 }
--- services/Vendor/src/test/java/com/bottega/vendor/concert/fixtures/TitleAssertSpock.groovy
+++ services/Vendor/src/test/java/com/bottega/vendor/concert/fixtures/TitleAssertSpock.groovy
@@ -6,6 +6,9 @@ import groovy.transform.TupleConstructor
 import io.vavr.control.Validation
 import spock.lang.Specification
 
+import static com.bottega.sharedlib.vo.error.ErrorType.BAD_REQUEST
+import static com.bottega.vendor.concert.api.app.ConcertErrorCode.invalid_title
+
 @TupleConstructor
 class TitleAssertSpock extends Specification {
 
@@ -21,4 +24,21 @@ class TitleAssertSpock extends Specification {
         this
     }
 
+    TitleAssertSpock hasInvalidLengthError() {
+        hasInvalidTitleError("Title length must be between 10 and 160 chars")
+    }
+
+    TitleAssertSpock hasBannedWordError(String expectedBannedWord) {
+        hasInvalidTitleError("Title must not contain banned word: " + expectedBannedWord)
+    }
+
+    protected TitleAssertSpock hasInvalidTitleError(String expectedErrorDesc) {
+        assert actualTitle.isInvalid()
+        with(actualTitle.getError()) {
+            type == BAD_REQUEST
+            code == invalid_title
+            description == expectedErrorDesc
+        }
+        this
+    }
 }
