bcefc4d1479b61f7a35c7ed32d6f60960ea6ebf7
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-11-15 11:57:56 +0100

    live: micro failing test

--- services/Vendor/src/test/java/com/bottega/vendor/concert/domain/ConcertDate_MicroTest.java
+++ services/Vendor/src/test/java/com/bottega/vendor/concert/domain/ConcertDate_MicroTest.java
@@ -3,15 +3,15 @@ package com.bottega.vendor.concert.domain;
 import com.bottega.vendor.concert.fixtures.ConcertLogicTestBase;
 import org.junit.jupiter.api.Test;
 
+import static java.time.LocalDate.of;
+import static org.assertj.core.api.Assertions.assertThat;
+
 class ConcertDate_MicroTest extends ConcertLogicTestBase {
 
     @Test
     public void fromString_OK_onDateString(){
-        //given
-
-        //when
-
-        //then
+        //expect
+        assertThat(ConcertDate.from("2022-02-22", sharedFixtures.clock).get().getUtcDate()).isEqualTo(of(2022, 2, 22));
     }
 
 }
