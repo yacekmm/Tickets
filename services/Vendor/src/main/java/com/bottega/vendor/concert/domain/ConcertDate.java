e0a8f090bded1e632662333c6ad52eec22fb0fea
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-11-15 12:31:17 +0100

    live: micro Parameterized test on validating date threshold

--- services/Vendor/src/main/java/com/bottega/vendor/concert/domain/ConcertDate.java
+++ services/Vendor/src/main/java/com/bottega/vendor/concert/domain/ConcertDate.java
@@ -11,6 +11,7 @@ import java.time.*;
 import static com.bottega.sharedlib.vo.error.ErrorResult.badRequest;
 import static com.bottega.vendor.concert.api.app.ConcertErrorCode.invalid_date;
 import static java.time.ZoneOffset.UTC;
+import static java.time.temporal.ChronoUnit.DAYS;
 
 @ValueObject
 @Embeddable
@@ -21,6 +22,8 @@ import static java.time.ZoneOffset.UTC;
 public class ConcertDate {
 
 
+    private static final int MIN_DATE_THRESHOLD_DAYS = 7;
+
     @Column(name = "date")
     private Instant date;
 
@@ -29,6 +32,8 @@ public class ConcertDate {
                 .orElse(Try.of(() -> Instant.parse(date)))
                 .toValidation()
                 .mapError(throwable -> badRequest(invalid_date, "Unsupported date format: %s", throwable.getMessage()))
+                .filter(instant -> instant.isAfter(clock.instant().plus(MIN_DATE_THRESHOLD_DAYS, DAYS)))
+                .getOrElse(Validation.invalid(badRequest(invalid_date, "Too early")))
                 .map(ConcertDate::new);
     }
 
--- services/Vendor/src/test/java/com/bottega/vendor/concert/domain/ConcertDate_MicroTest.java
+++ services/Vendor/src/test/java/com/bottega/vendor/concert/domain/ConcertDate_MicroTest.java
@@ -4,13 +4,29 @@ import com.bottega.sharedlib.vo.error.ErrorResult;
 import com.bottega.vendor.concert.fixtures.ConcertLogicTestBase;
 import io.vavr.control.Validation;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
 
+import java.util.stream.Stream;
+
+import static com.bottega.sharedlib.config.TestClockConfig.TEST_TIME;
 import static com.bottega.sharedlib.vo.error.ErrorType.BAD_REQUEST;
 import static com.bottega.vendor.concert.api.app.ConcertErrorCode.invalid_date;
 import static java.time.LocalDate.of;
+import static java.time.temporal.ChronoUnit.DAYS;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class ConcertDate_MicroTest extends ConcertLogicTestBase {
+
+    public static Stream<Arguments> tooEarlyDatesSource() {
+        return Stream.of(
+                arguments(TEST_TIME.minus(1, DAYS).toString()),
+                arguments(TEST_TIME.toString()),
+                arguments(TEST_TIME.plus(6, DAYS).toString()),
+                arguments(TEST_TIME.plus(7, DAYS).toString())
+        );
+    }
 
     @Test
     public void fromString_OK_onDateString(){
@@ -34,5 +50,23 @@ class ConcertDate_MicroTest extends ConcertLogicTestBase {
         assertThat(result.getError().getType()).isEqualTo(BAD_REQUEST);
         assertThat(result.getError().getCode()).isEqualTo(invalid_date);
         assertThat(result.getError().getDescription()).startsWith("Unsupported date format:");
+    }
+
+    @ParameterizedTest
+    @MethodSource("tooEarlyDatesSource")
+    public void fromString_returnsError_onDateUnderMinimumThreshold(String invalidDate){
+        //when
+        Validation<ErrorResult, ConcertDate> result = ConcertDate.from(invalidDate, sharedFixtures.clock);
+
+        //then
+        assertThat(result.getError().getType()).isEqualTo(BAD_REQUEST);
+        assertThat(result.getError().getCode()).isEqualTo(invalid_date);
+        assertThat(result.getError().getDescription()).startsWith("Too early");
+    }
+
+    @Test
+    public void fromString_OK_onDateRightOverThreshold(){
+        //expect
+        assertThat(ConcertDate.from(TEST_TIME.plus(8, DAYS).toString(), sharedFixtures.clock).get().getUtcDate()).isEqualTo(of(2022, 2, 13));
     }
 }
