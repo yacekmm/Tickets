cfe7a76c6161b61fd7c1299385381d6a50978b2e
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-11-16 08:51:38 +0100

    comp-http: client

--- services/Vendor/src/test/java/com/bottega/vendor/fixtures/FakePricingClient.java
+++ services/Vendor/src/test/java/com/bottega/vendor/fixtures/FakePricingClient.java
@@ -1,17 +1,27 @@
 package com.bottega.vendor.fixtures;
 
+import com.bottega.sharedlib.vo.Money;
 import com.bottega.sharedlib.vo.error.ErrorResult;
-import com.bottega.vendor.concert.Price;
+import com.bottega.vendor.concert.*;
 import com.bottega.vendor.concert.domain.ConcertId;
 import com.bottega.vendor.infra.client.pricing.PricingClient;
 import io.vavr.control.Either;
 
 import java.util.List;
 
+import static java.util.List.of;
+
 public class FakePricingClient implements PricingClient {
 
     @Override
     public Either<ErrorResult, List<Price>> applyPercentageDiscount(ConcertId itemId, int percentage) {
-        return null;
+        return Either.right(
+                of(new Price(
+                        new Money(90_00),
+                        of(new PriceFactor(
+                                null,
+                                percentage,
+                                null))))
+        );
     }
 }
