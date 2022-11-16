18789a1fd053078f2e2c54fc1120db6d016fa8f6
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-11-16 14:10:31 +0100

    e2e: start

--- SystemTests/TicketsTests/src/test/java/com/bottega/tickets/concert/CreateConcert_SystemTest.java
+++ SystemTests/TicketsTests/src/test/java/com/bottega/tickets/concert/CreateConcert_SystemTest.java
@@ -1,19 +1,20 @@
 package com.bottega.tickets.concert;
 
-import com.bottega.tickets.concert.fixtures.ConcertOperations;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 
-import java.time.LocalDateTime;
+import java.time.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
 
 public class CreateConcert_SystemTest {
 
     private SeleniumFixtures seleniumFixtures;
-    private ConcertOperations concertOperations;
 
     @BeforeEach
     void setUp() {
         seleniumFixtures = SeleniumFixtures.init("http://localhost:4200");
-        concertOperations = ConcertOperations.init(seleniumFixtures);
     }
 
     @AfterEach
@@ -25,13 +26,30 @@ public class CreateConcert_SystemTest {
     void createsConcert() {
         String concertTitle = "GUI test concert " + LocalDateTime.now();
 
+        seleniumFixtures.driver.findElement(By.id("menu-concerts-add")).click();
+        seleniumFixtures.driver.findElement(By.id("input-concert-title")).sendKeys(concertTitle);
+        seleniumFixtures.driver.findElement(By.className("mat-calendar-next-button")).click();
+        seleniumFixtures.driver.findElement(By.className("mat-calendar-next-button")).click();
+        seleniumFixtures.driver.findElements(By.className("mat-calendar-body-cell-content")).stream()
+                .filter(webElement -> webElement.getText().contains("10"))
+                .findFirst()
+                .orElseThrow()
+                .click();
+        seleniumFixtures.driver.findElement(By.id("create-concert-submit")).click();
+        await().untilAsserted(() -> assertThat(seleniumFixtures.driver.findElement((By.className("mat-simple-snackbar"))).isDisplayed()).isTrue());
 
-        concertOperations
-                .openCreateConcertView()
-                .createConcert(concertTitle, 2, 10)
-                .assertCreationSuccess()
-                .openConcertListView()
-                .assertNewConcertIsListed(concertTitle, 2, 10);
+        seleniumFixtures.driver.findElement(By.id("menu-concerts-list")).click();
+        String date = seleniumFixtures.driver.findElement(By.id("concerts-list"))
+                .findElements(By.className("mat-row")).stream()
+                .filter(webElement -> webElement.findElement(By.className("concert-title")).getText().equals(concertTitle))
+                .findFirst()
+                .orElseThrow()
+                .findElement(By.className("concert-date"))
+                .getText();
+
+        LocalDate parsedDate = LocalDate.parse(date);
+        LocalDate expectedDate = LocalDate.now().plusMonths(2).withDayOfMonth(10);
+        assertThat(parsedDate).isEqualTo(expectedDate);
     }
 
 }
