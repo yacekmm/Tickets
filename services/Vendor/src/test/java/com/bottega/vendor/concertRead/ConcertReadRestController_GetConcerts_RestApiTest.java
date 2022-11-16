56d4f3482ff8e78f311e063227c14569eeb749f8
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-11-16 11:21:06 +0100

    api-read: when

--- services/Vendor/src/test/java/com/bottega/vendor/concertRead/ConcertReadRestController_GetConcerts_RestApiTest.java
+++ services/Vendor/src/test/java/com/bottega/vendor/concertRead/ConcertReadRestController_GetConcerts_RestApiTest.java
@@ -1,7 +1,11 @@
 package com.bottega.vendor.concertRead;
 
 import com.bottega.vendor.fixtures.FrameworkTestBase;
+import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.Test;
+
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.hamcrest.Matchers.*;
 
 public class ConcertReadRestController_GetConcerts_RestApiTest extends FrameworkTestBase {
 
@@ -11,9 +15,14 @@ public class ConcertReadRestController_GetConcerts_RestApiTest extends Framework
         //given
 
         //when
+        ValidatableResponse response = concertFixtures.concertHttpClient.findConcertsForVendor("vendor");
 
         //then
-
+        response
+                .statusCode(SC_OK)
+                .body("$", hasSize(2))
+                .body("[0].id", equalTo(concert_1.getId().asString()))
+                .body("[1].id", equalTo(concert_2.getId().asString()));
     }
 
 }
