4fd5c142ff431ae2f6d19977907b2ee35f0b5f34
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-11-16 13:47:27 +0100

    live-api-gui-start: fragile tests

--- services/vendor-ui/src/e2e/test/concerts/concert.e2e-spec.ts
+++ services/vendor-ui/src/e2e/test/concerts/concert.e2e-spec.ts
@@ -1,33 +1,37 @@
-import {ConcertOperations} from "./concert-operations";
-import {BrowserTools} from "../shared/browser-tools";
+import {browser, By, element, protractor} from "protractor";
 
-describe('concert', () => {
-
-  let browserTools: BrowserTools;
-  let concertOperations: ConcertOperations;
+describe('ui', () => {
 
   beforeEach(() => {
-    browserTools = new BrowserTools()
-    concertOperations = new ConcertOperations(browserTools)
-    browserTools.setup();
+    browser.ignoreSynchronization = true;
+    browser.get('http://localhost:4200/');
   });
 
   it('opens page', () => {
-    concertOperations
-      .assertPageIsOpened();
+    browser.driver.getTitle().then(pageTitle => expect(pageTitle).toEqual('Vendor UI'));
   });
 
   it('creates concert', function () {
-    concertOperations
-      .openAddConcert()
-      .createConcert()
-      .assertCreateConcertSuccess();
+    element(By.id("menu-concerts-add")).click()
+    element(By.id("input-concert-title")).sendKeys("concert Title");
+    element(By.id("create-concert-submit")).click();
+    let successConfirmation = element(By.className("mat-simple-snackbar"));
+    let expected = protractor.ExpectedConditions;
+    browser.wait(expected.visibilityOf(successConfirmation))
+      .then(unused =>
+        expect(successConfirmation.isDisplayed()).toBeTruthy()
+      );
   });
 
   it('lists stub concerts', () => {
-    concertOperations
-      .openListConcerts()
-      .assertConcertsListed(['Rihanna in Rome', 'Rock concert 2']);
+    element(By.id("menu-concerts-list")).click();
+
+    element.all(By.className('mat-row'))
+      .all(By.className('concert-title'))
+      .getText()
+      .then(value =>
+        expect(value).toEqual(['Rihanna in Rome', 'Rock concert 2'])
+      );
   });
 
 
