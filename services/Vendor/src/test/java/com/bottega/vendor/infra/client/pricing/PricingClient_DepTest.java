fe707e46479671b064fab974b7ea15c36677e45e
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-11-16 13:01:47 +0100

    dep-http: when

--- services/Vendor/src/test/java/com/bottega/vendor/infra/client/pricing/PricingClient_DepTest.java
+++ services/Vendor/src/test/java/com/bottega/vendor/infra/client/pricing/PricingClient_DepTest.java
@@ -1,7 +1,13 @@
 package com.bottega.vendor.infra.client.pricing;
 
+import com.bottega.sharedlib.vo.error.ErrorResult;
+import com.bottega.vendor.concert.Price;
+import com.bottega.vendor.concert.domain.ConcertId;
 import com.bottega.vendor.fixtures.*;
+import io.vavr.control.Either;
 import org.junit.jupiter.api.*;
+
+import java.util.List;
 
 public class PricingClient_DepTest extends FrameworkTestBase {
 
@@ -17,8 +23,11 @@ public class PricingClient_DepTest extends FrameworkTestBase {
     @Test
     public void fakeApplyDiscountClient_isValid() {
         //given
+        ConcertId concertId = new ConcertId();
 
         //when
+        Either<ErrorResult, List<Price>> realResult = realPricingClient.applyPercentageDiscount(concertId, 10);
+        Either<ErrorResult, List<Price>> fakeResult = fakePricingClient.applyPercentageDiscount(concertId, 10);
 
         //then
 
