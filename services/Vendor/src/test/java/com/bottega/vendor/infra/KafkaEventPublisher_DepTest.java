9149dafc12ff548b0070b577114de5697a002098
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-11-16 12:29:20 +0100

    dep-kafka: when

--- services/Vendor/src/test/java/com/bottega/vendor/infra/KafkaEventPublisher_DepTest.java
+++ services/Vendor/src/test/java/com/bottega/vendor/infra/KafkaEventPublisher_DepTest.java
@@ -1,15 +1,22 @@
 package com.bottega.vendor.infra;
 
+import com.bottega.sharedlib.event.Event;
+import com.bottega.vendor.concert.domain.Concert;
 import com.bottega.vendor.fixtures.FrameworkTestBase;
 import org.junit.jupiter.api.Test;
+
+import static com.bottega.vendor.concert.domain.VendorEventFactory.concertCreated;
 
 class KafkaEventPublisher_DepTest extends FrameworkTestBase {
 
     @Test
     public void publishEvent_OK(){
         //given
+        Concert concert = builders.aConcert().build();
+        Event concertCreated = concertCreated(concert, 2);
 
         //when
+        sharedFixtures.eventPublisher.publish(concertCreated);
 
         //then
 
