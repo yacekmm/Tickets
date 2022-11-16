0ed47e549c1cf82fe1dac941fbffea34f5292028
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-11-16 11:19:16 +0100

    api-read: start

---
+++ services/Vendor/src/test/java/com/bottega/vendor/concertRead/ConcertReadRestController_GetConcerts_RestApiTest.java
@@ -0,0 +1,19 @@
+package com.bottega.vendor.concertRead;
+
+import com.bottega.vendor.fixtures.FrameworkTestBase;
+import org.junit.jupiter.api.Test;
+
+public class ConcertReadRestController_GetConcerts_RestApiTest extends FrameworkTestBase {
+
+
+    @Test
+    public void findConcerts_returnsSortedConcertsForVendor_onValidRequest() {
+        //given
+
+        //when
+
+        //then
+
+    }
+
+}
