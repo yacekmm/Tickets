16a972ed64a57543590656acd195fd625e32cad9
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-11-30 15:24:24 +0100

    live-micro-sample

--- services/Vendor/src/test/java/com/bottega/vendor/concert/domain/ConcertDate_fromString_microTest.java
+++ services/Vendor/src/test/java/com/bottega/vendor/concert/domain/ConcertDate_fromString_microTest.java
@@ -2,16 +2,14 @@ package com.bottega.vendor.concert.domain;
 
 import com.bottega.vendor.concert.fixtures.ConcertLogicTestBase;
 import org.junit.jupiter.api.Test;
+import static java.time.LocalDate.of;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ConcertDate_fromString_microTest extends ConcertLogicTestBase {
 
     @Test
     public void fromString_OK_onDateString(){
-        //given
-
-        //when
-
-        //then
-
+        //expect
+        assertThat(ConcertDate.from("2022-02-22", sharedFixtures.clock).get().getUtcDate()).isEqualTo(of(2022, 2, 22));
     }
 }
