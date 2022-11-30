63065061f7b8dba348043aa269a21421033b0b8a
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-11-30 15:33:58 +0100

    micro-sample

--- services/Vendor/src/test/java/com/bottega/vendor/concert/domain/Title_fromString_microTest.java
+++ services/Vendor/src/test/java/com/bottega/vendor/concert/domain/Title_fromString_microTest.java
@@ -2,11 +2,14 @@ package com.bottega.vendor.concert.domain;
 
 import com.bottega.vendor.concert.fixtures.ConcertLogicTestBase;
 import org.junit.jupiter.api.Test;
+import static com.bottega.vendor.concert.domain.Title.from;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class Title_fromString_microTest extends ConcertLogicTestBase {
 
     @Test
     public void fromString_OK_onValidString(){
         //expect
+        assertThat(from("This is a valid title").get().getValue()).isEqualTo("This is a valid title");
     }
 }
