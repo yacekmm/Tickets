654c610d67c3c1a151015d4689130e11df5153e7
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-11-30 15:27:35 +0100

    skip

--- services/Vendor/src/main/java/com/bottega/vendor/concert/domain/ConcertDate.java
+++ services/Vendor/src/main/java/com/bottega/vendor/concert/domain/ConcertDate.java
@@ -5,9 +5,12 @@ import java.time.*;
 
 import com.bottega.sharedlib.ddd.ValueObject;
 import com.bottega.sharedlib.vo.error.ErrorResult;
-import io.vavr.control.Validation;
+import io.vavr.control.*;
 import lombok.*;
+import static com.bottega.sharedlib.vo.error.ErrorResult.badRequest;
+import static com.bottega.vendor.concert.api.app.ConcertErrorCode.invalid_date;
 import static java.time.ZoneOffset.UTC;
+import static java.time.temporal.ChronoUnit.DAYS;
 
 @ValueObject
 @Embeddable
@@ -18,11 +21,19 @@ import static java.time.ZoneOffset.UTC;
 public class ConcertDate {
 
 
+    private static final int MIN_DATE_THRESHOLD_DAYS = 7;
+
     @Column(name = "date")
     private Instant date;
 
     public static Validation<ErrorResult, ConcertDate> from(String date, Clock clock) {
-        return Validation.valid(new ConcertDate(LocalDate.parse(date).atStartOfDay().toInstant(UTC)));
+        return Try.of(() -> LocalDate.parse(date).atStartOfDay().toInstant(UTC))
+                .orElse(Try.of(() -> Instant.parse(date)))
+                .toValidation()
+                .mapError(throwable -> badRequest(invalid_date, "Unsupported date format: %s", throwable.getMessage()))
+                .filter(instant -> instant.isAfter(clock.instant().plus(MIN_DATE_THRESHOLD_DAYS, DAYS)))
+                .getOrElse(Validation.invalid(badRequest(invalid_date, "Too early")))
+                .map(ConcertDate::new);
     }
 
     public LocalDate getUtcDate() {
