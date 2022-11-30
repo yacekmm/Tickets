b2fde37479b5ef69a88217576f1a3b5174776285
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-11-30 15:31:15 +0100

    micro-start-test-after

--- services/Vendor/src/main/java/com/bottega/vendor/concert/domain/Title.java
+++ services/Vendor/src/main/java/com/bottega/vendor/concert/domain/Title.java
@@ -1,13 +1,16 @@
 package com.bottega.vendor.concert.domain;
 
 import javax.persistence.*;
-import java.util.List;
+import java.util.*;
 
 import com.bottega.sharedlib.ddd.ValueObject;
 import com.bottega.sharedlib.vo.error.ErrorResult;
 import io.vavr.control.Validation;
 import lombok.*;
+import static com.bottega.sharedlib.vo.error.ErrorResult.badRequest;
+import static com.bottega.vendor.concert.api.app.ConcertErrorCode.invalid_title;
 import static java.util.List.of;
+import static org.apache.commons.lang3.StringUtils.*;
 
 @ValueObject
 @Embeddable
@@ -28,6 +31,22 @@ public class Title {
     private String value;
 
     public static Validation<ErrorResult, Title> from(String title) {
-        return null;
+
+        String trimmed = defaultString(trim(title), "");
+
+        Optional<String> bannedWord = BANNED_WORDS.stream()
+                .filter(word -> containsIgnoreCase(trimmed, word))
+                .findFirst();
+
+        if (bannedWord.isPresent()) {
+            return Validation.invalid(badRequest(invalid_title, "Title must not contain banned word: %s", bannedWord.get()));
+        }
+
+        if (length(trimmed) < TITLE_MIN_LEN || length(trimmed) > TITLE_MAX_LEN) {
+            return Validation.invalid(badRequest(invalid_title, "Title length must be between %s and %s chars", TITLE_MIN_LEN, TITLE_MAX_LEN));
+        }
+
+        return Validation.valid(new Title(trimmed));
+
     }
 }
