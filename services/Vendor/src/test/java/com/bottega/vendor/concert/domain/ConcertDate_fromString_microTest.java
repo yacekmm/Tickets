43835af4641f9fec8bde0ef6dd91746a452c500f
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-11-30 16:00:33 +0100

    live-micro-assert-sample

--- services/Vendor/src/test/java/com/bottega/vendor/concert/domain/ConcertDate_fromString_microTest.java
+++ services/Vendor/src/test/java/com/bottega/vendor/concert/domain/ConcertDate_fromString_microTest.java
@@ -3,7 +3,7 @@ package com.bottega.vendor.concert.domain;
 import java.util.stream.Stream;
 
 import com.bottega.sharedlib.vo.error.ErrorResult;
-import com.bottega.vendor.concert.fixtures.ConcertLogicTestBase;
+import com.bottega.vendor.concert.fixtures.*;
 import io.vavr.control.Validation;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -11,6 +11,7 @@ import org.junit.jupiter.params.provider.*;
 import static com.bottega.sharedlib.config.TestClockConfig.TEST_TIME;
 import static com.bottega.sharedlib.vo.error.ErrorType.BAD_REQUEST;
 import static com.bottega.vendor.concert.api.app.ConcertErrorCode.invalid_date;
+import static com.bottega.vendor.concert.domain.ConcertDate.from;
 import static java.time.LocalDate.of;
 import static java.time.temporal.ChronoUnit.DAYS;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -30,13 +31,15 @@ class ConcertDate_fromString_microTest extends ConcertLogicTestBase {
     @Test
     public void fromString_OK_onDateString(){
         //expect
-        assertThat(ConcertDate.from("2022-02-22", sharedFixtures.clock).get().getUtcDate()).isEqualTo(of(2022, 2, 22));
+        ConcertDateAssert.assertThatConcertDate(from("2022-02-22", sharedFixtures.clock)).isEqualTo(2022, 2, 22);
+        ConcertDateAssert.assertThatConcertDate(from("2025-01-01", sharedFixtures.clock)).isEqualTo(2025, 1, 1);
+        ConcertDateAssert.assertThatConcertDate(from("2025-12-31", sharedFixtures.clock)).isEqualTo(2025, 12, 31);
     }
 
     @Test
     public void fromString_OK_onDateTimeString(){
         //expect
-        assertThat(ConcertDate.from("2022-02-22T22:11:00Z", sharedFixtures.clock).get().getUtcDate()).isEqualTo(of(2022, 2, 22));
+        ConcertDateAssert.assertThatConcertDate(from("2022-02-27T07:20:00Z", sharedFixtures.clock)).isEqualTo(2022, 2, 27);
     }
 
 
