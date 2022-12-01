fdaec3e27f41141b92022088ada451f07b2d1a3f
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-12-01 08:54:00 +0100

    live-unit-real

--- services/Vendor/src/test/java/com/bottega/vendor/concert/domain/Concert_initConcert_unitTest.java
+++ services/Vendor/src/test/java/com/bottega/vendor/concert/domain/Concert_initConcert_unitTest.java
@@ -8,7 +8,6 @@ import com.bottega.vendor.concert.fixtures.ConcertLogicTestBase;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
 
 class Concert_initConcert_unitTest extends ConcertLogicTestBase {
 
@@ -28,7 +27,7 @@ class Concert_initConcert_unitTest extends ConcertLogicTestBase {
     public void initConcert_assignsCategory(String title, String expectedCategory) {
         //given
         Concert newConcert = new Concert(new ConcertId(), Title.from(title).get(), ConcertDate.from(TestClockConfig.TEST_TIME_PLUS_30_DAYS.toString(), sharedFixtures.clock).get(), "vendor-id", new HashSet<>(), null);
-        CategoryService categoryService = mock(CategoryService.class);
+        CategoryService categoryService = new CategoryService();
 
         //when
         newConcert.initNewConcert(categoryService);
