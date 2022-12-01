bd23bb5a96c1aa095855b437c3611815bbbbf023
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-12-01 09:06:55 +0100

    unit-done

--- services/Vendor/src/test/java/com/bottega/vendor/concert/domain/Concert_initConcert_unitSpec.groovy
+++ services/Vendor/src/test/java/com/bottega/vendor/concert/domain/Concert_initConcert_unitSpec.groovy
@@ -1,13 +1,15 @@
 package com.bottega.vendor.concert.domain
 
-import com.bottega.sharedlib.config.TestClockConfig
+
 import com.bottega.vendor.fixtures.SpecificationBase
+
+import static com.bottega.sharedlib.config.TestClockConfig.TEST_TIME_PLUS_30_DAYS
 
 class Concert_initConcert_unitSpec extends SpecificationBase {
 
     def "initConcert - adds tags"() {
         given:
-        def newConcert = new Concert(new ConcertId(), Title.from(title).get(), ConcertDate.from(TestClockConfig.TEST_TIME_PLUS_30_DAYS.toString(), sharedFixtures.clock).get(), "vendor-id", new HashSet<>(), null)
+        def newConcert = new Concert(new ConcertId(), Title.from(title).get(), ConcertDate.from(TEST_TIME_PLUS_30_DAYS.toString(), sharedFixtures.clock).get(), "vendor-id", new HashSet<>(), null)
 
         when:
         newConcert.initNewConcert(concertFixtures.tagService, concertFixtures.categoryService)
@@ -21,11 +23,15 @@ class Concert_initConcert_unitSpec extends SpecificationBase {
         where:
         title                          | expectedTags
         "no tags apply"                | []
+        "Rock concert"                 | ["rock"]
+        "Scorpions in Warsaw!"         | ["rock"]
+        "Scorpions on Mystic Festival" | ["rock", "festival"]
+        "Rihanna the best of"          | ["pop"]
     }
 
     def "initConcert - assigns category"() {
         given:
-        def newConcert = new Concert(new ConcertId(), Title.from(title).get(), ConcertDate.from(TestClockConfig.TEST_TIME_PLUS_30_DAYS.toString(), sharedFixtures.clock).get(), "vendor-id", new HashSet<>(), null)
+        def newConcert = new Concert(new ConcertId(), Title.from(title).get(), ConcertDate.from(TEST_TIME_PLUS_30_DAYS.toString(), sharedFixtures.clock).get(), "vendor-id", new HashSet<>(), null)
 
         when:
         newConcert.initNewConcert(concertFixtures.tagService, concertFixtures.categoryService)
--- services/Vendor/src/test/java/com/bottega/vendor/concert/domain/Concert_initConcert_unitTest.java
+++ services/Vendor/src/test/java/com/bottega/vendor/concert/domain/Concert_initConcert_unitTest.java
@@ -3,10 +3,10 @@ package com.bottega.vendor.concert.domain;
 import java.util.*;
 import java.util.stream.Stream;
 
-import com.bottega.sharedlib.config.TestClockConfig;
 import com.bottega.vendor.concert.fixtures.ConcertLogicTestBase;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
+import static com.bottega.sharedlib.config.TestClockConfig.TEST_TIME_PLUS_30_DAYS;
 import static java.util.stream.Collectors.toSet;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,7 +14,11 @@ class Concert_initConcert_unitTest extends ConcertLogicTestBase {
 
     private static Stream<Arguments> provideStringsForTags() {
         return Stream.of(
-                Arguments.of("no tags apply", Set.of())
+                Arguments.of("no tags apply", Set.of()),
+                Arguments.of("Rock concert", Set.of("rock")),
+                Arguments.of("Scorpions in Warsaw!", Set.of("rock")),
+                Arguments.of("Scorpions on Mystic Festival", Set.of("rock", "festival")),
+                Arguments.of("Rihanna the best of", Set.of("pop"))
         );
     }
 
@@ -22,7 +26,7 @@ class Concert_initConcert_unitTest extends ConcertLogicTestBase {
     @MethodSource("provideStringsForTags")
     void initConcert_addsTags(String title, Set<String> expectedTags) {
         //given
-        Concert newConcert = new Concert(new ConcertId(), Title.from(title).get(), ConcertDate.from(TestClockConfig.TEST_TIME_PLUS_30_DAYS.toString(), sharedFixtures.clock).get(), "vendor-id", new HashSet<>(), null);
+        Concert newConcert = new Concert(new ConcertId(), Title.from(title).get(), ConcertDate.from(TEST_TIME_PLUS_30_DAYS.toString(), sharedFixtures.clock).get(), "vendor-id", new HashSet<>(), null);
 
         //when
         newConcert.initNewConcert(concertFixtures.tagService, concertFixtures.categoryService);
@@ -32,7 +36,7 @@ class Concert_initConcert_unitTest extends ConcertLogicTestBase {
     }
 
 
-    public static Stream<Arguments> provideStringsForCategories() {
+    private static Stream<Arguments> provideStringsForCategories() {
         return Stream.of(
                 Arguments.of("no category", "other"),
                 Arguments.of("Rock concert", "rock"),
@@ -44,9 +48,9 @@ class Concert_initConcert_unitTest extends ConcertLogicTestBase {
 
     @ParameterizedTest
     @MethodSource("provideStringsForCategories")
-    public void initConcert_assignsCategory(String title, String expectedCategory) {
+    void initConcert_assignsCategory(String title, String expectedCategory) {
         //given
-        Concert newConcert = new Concert(new ConcertId(), Title.from(title).get(), ConcertDate.from(TestClockConfig.TEST_TIME_PLUS_30_DAYS.toString(), sharedFixtures.clock).get(), "vendor-id", new HashSet<>(), null);
+        Concert newConcert = new Concert(new ConcertId(), Title.from(title).get(), ConcertDate.from(TEST_TIME_PLUS_30_DAYS.toString(), sharedFixtures.clock).get(), "vendor-id", new HashSet<>(), null);
 
         //when
         newConcert.initNewConcert(concertFixtures.tagService, concertFixtures.categoryService);
@@ -54,4 +58,5 @@ class Concert_initConcert_unitTest extends ConcertLogicTestBase {
         //then
         assertThat(newConcert.getCategory().getValue()).isEqualTo(expectedCategory);
     }
+
 }
