7e4046b53ba2189b34421f3b06b4e65fb71c1b3a
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-12-01 08:51:07 +0100

    live-unit-inline

--- services/Vendor/src/test/java/com/bottega/vendor/concert/domain/Concert_initConcert_unitTest.java
+++ services/Vendor/src/test/java/com/bottega/vendor/concert/domain/Concert_initConcert_unitTest.java
@@ -1,8 +1,37 @@
 package com.bottega.vendor.concert.domain;
 
+import java.util.HashSet;
+import java.util.stream.Stream;
+
+import com.bottega.sharedlib.config.TestClockConfig;
 import com.bottega.vendor.concert.fixtures.ConcertLogicTestBase;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class Concert_initConcert_unitTest extends ConcertLogicTestBase {
 
 
+    public static Stream<Arguments> provideStringsForCategories() {
+        return Stream.of(
+                Arguments.of("no category", "other"),
+                Arguments.of("Rock concert", "rock"),
+                Arguments.of("Scorpions in Warsaw", "rock"),
+                Arguments.of("Scorpions on Mystic Festival", "rock"),
+                Arguments.of("Rihanna the best of", "superstar")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideStringsForCategories")
+    public void initConcert_assignsCategory(String title, String expectedCategory) {
+        //given
+        Concert newConcert = new Concert(new ConcertId(), Title.from(title).get(), ConcertDate.from(TestClockConfig.TEST_TIME_PLUS_30_DAYS.toString(), sharedFixtures.clock).get(), "vendor-id", new HashSet<>(), null);
+
+        //when
+        newConcert.initNewConcert();
+
+        //then
+        assertThat(newConcert.getCategory().getValue()).isEqualTo(expectedCategory);
+    }
 }
