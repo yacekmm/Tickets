e395c5db29aec474f0dac1beaad18a547da1fc5e
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-12-01 08:51:07 +0100

    live-unit-inline

--- services/Vendor/src/main/java/com/bottega/vendor/concert/domain/Concert.java
+++ services/Vendor/src/main/java/com/bottega/vendor/concert/domain/Concert.java
@@ -1,12 +1,13 @@
 package com.bottega.vendor.concert.domain;
 
 import javax.persistence.*;
-import java.util.Set;
+import java.util.*;
 
 import com.bottega.sharedlib.ddd.AggregateRoot;
 import com.bottega.sharedlib.repo.BaseEntity;
 import com.bottega.vendor.agreements.VendorId;
 import lombok.*;
+import org.apache.commons.lang3.StringUtils;
 import static javax.persistence.CascadeType.*;
 import static javax.persistence.FetchType.LAZY;
 import static lombok.AccessLevel.*;
@@ -22,19 +23,22 @@ import static lombok.AccessLevel.*;
 @Builder(access = PUBLIC)
 public class Concert implements BaseEntity {
 
+    private static final Map<String, String[]> defaultCategories = new HashMap<>();
+
+    static {
+        defaultCategories.put("rock", new String[]{"Rock", "Scorpions"});
+        defaultCategories.put("superstar", new String[]{"Rihanna"});
+    }
+
     @EmbeddedId
     @EqualsAndHashCode.Include
     private ConcertId id;
-
     @Embedded
     private Title title;
-
     @Embedded
     private ConcertDate date;
-
     @Getter(NONE)
     private String vendorId;
-
     @ManyToMany(fetch = LAZY, cascade = PERSIST)
     @JoinTable(
             name = "concert_tags",
@@ -42,14 +46,19 @@ public class Concert implements BaseEntity {
             inverseJoinColumns = { @JoinColumn(name = "tag_id") }
     )
     private Set<Tag> tags;
-
     @ManyToOne(fetch = LAZY, cascade = ALL)
     @JoinColumn(name = "concerts")
     private Category category;
-
 
     public VendorId vendorId() {
         return new VendorId(vendorId);
     }
 
+    public void initNewConcert() {
+        category = defaultCategories.entrySet().stream()
+                .filter(entry -> StringUtils.containsAnyIgnoreCase(title.getValue(), entry.getValue()))
+                .findFirst()
+                .map(entry -> Category.from(entry.getKey()))
+                .orElse(Category.from("other"));
+    }
 }
