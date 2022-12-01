a0464483f20e948b24365e3316d06f5c1e40b39f
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-12-01 08:51:07 +0100

    live-unit-inline

--- services/Vendor/src/test/java/com/bottega/vendor/concert/domain/Concert_initConcert_unitSpec.groovy
+++ services/Vendor/src/test/java/com/bottega/vendor/concert/domain/Concert_initConcert_unitSpec.groovy
@@ -1,8 +1,27 @@
 package com.bottega.vendor.concert.domain
 
+import com.bottega.sharedlib.config.TestClockConfig
 import com.bottega.vendor.fixtures.SpecificationBase
 
 class Concert_initConcert_unitSpec extends SpecificationBase {
 
+    def "initConcert - assigns category"() {
+        given:
+        def concert = new Concert(new ConcertId(), Title.from(title).get(), ConcertDate.from(TestClockConfig.TEST_TIME_PLUS_30_DAYS.toString(), sharedFixtures.clock).get(), "vendor-id", new HashSet<>(), null)
+
+        when:
+        concert.initNewConcert()
+
+        then:
+        concert.getCategory().value == expectedCategory
+
+        where:
+        title                          | expectedCategory
+        "no category"                  | "other"
+        "Rock concert"                 | "rock"
+        "Scorpions in Warsaw!"         | "rock"
+        "Scorpions on Mystic Festival" | "rock"
+        "Rihanna the best of"          | "superstar"
+    }
 
 }
