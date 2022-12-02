4bcc94e5e0dfce208c6a3d55604314da4aad9fe7
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-12-02 12:17:47 +0100

    live-api-err-start

--- libs/SharedLib/src/test/java/com/bottega/sharedlib/fixtures/ErrorJsonAssert.java
+++ libs/SharedLib/src/test/java/com/bottega/sharedlib/fixtures/ErrorJsonAssert.java
@@ -3,10 +3,9 @@ package com.bottega.sharedlib.fixtures;
 import com.bottega.sharedlib.vo.error.*;
 import io.restassured.response.ValidatableResponse;
 import lombok.RequiredArgsConstructor;
-
-import static com.bottega.sharedlib.vo.error.ErrorType.*;
-import static com.bottega.sharedlib.vo.error.GenericErrorCode.*;
-import static org.apache.http.HttpStatus.*;
+import static com.bottega.sharedlib.vo.error.ErrorType.BAD_REQUEST;
+import static com.bottega.sharedlib.vo.error.GenericErrorCode.invalid_request;
+import static org.apache.http.HttpStatus.SC_BAD_REQUEST;
 import static org.hamcrest.Matchers.equalTo;
 
 @RequiredArgsConstructor(staticName = "assertThatError")
@@ -15,7 +14,6 @@ public class ErrorJsonAssert {
     private final ValidatableResponse errorResponse;
 
     public ErrorJsonAssert hasType(ErrorType expectedType) {
-        errorResponse.statusCode(expectedType.getHttpStatus().value());
         errorResponse.body("type", equalTo(expectedType.name()));
         return this;
     }
@@ -28,13 +26,6 @@ public class ErrorJsonAssert {
     public ErrorJsonAssert hasDescription(String expectedDesc) {
         errorResponse.body("description", equalTo(expectedDesc));
         return this;
-    }
-
-    public ErrorJsonAssert isNotFound(String expectedDescription) {
-        errorResponse.statusCode(SC_NOT_FOUND);
-        return hasType(NOT_FOUND)
-                .hasCode(not_found)
-                .hasDescription(expectedDescription);
     }
 
     public ErrorJsonAssert isBadRequest(String expectedDesc) {
