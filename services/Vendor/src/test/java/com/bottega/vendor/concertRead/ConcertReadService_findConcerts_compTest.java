6b3a980a426016b4b887c964952981f82006ac26
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-12-02 11:03:30 +0100

    unit-builder-start

--- services/Vendor/src/test/java/com/bottega/vendor/concertRead/ConcertReadService_findConcerts_compTest.java
+++ services/Vendor/src/test/java/com/bottega/vendor/concertRead/ConcertReadService_findConcerts_compTest.java
@@ -13,10 +13,9 @@ public class ConcertReadService_findConcerts_compTest extends ConcertLogicTestBa
 
     @Test
     public void findConcerts_returnsSortedConcertsForVendor_onValidRequest() {
-        //given
-        Concert concert_1 = builders.aConcert().withDate(TEST_TIME_PLUS_30_DAYS).withVendorId("vendor").inFinderDb();
-        Concert concert_2 = builders.aConcert().withDate(TEST_TIME_PLUS_60_DAYS).withVendorId("vendor").inFinderDb();
-        builders.aConcert().withVendorId("other").inDb();
+        Concert concert_1 = builders.dontLook().withDate(TEST_TIME_PLUS_30_DAYS).withVendorId("vendor").inFinderDb();
+        Concert concert_2 = builders.dontLook().withDate(TEST_TIME_PLUS_60_DAYS).withVendorId("vendor").inFinderDb();
+        builders.dontLook().withVendorId("other").inDb();
 
         //when
         List<Concert> result = concertReadFixtures.concertReadService.findConcertsForVendor("vendor");
