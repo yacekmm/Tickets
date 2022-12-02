1daca76dd0dcace2386314863edb2a4b5ccac5cc
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-12-02 13:09:37 +0100

    e2e-system-concert-operations

--- SystemTests/TicketsTests/src/test/java/com/bottega/tickets/concert/fixtures/ConcertOperations.java
+++ SystemTests/TicketsTests/src/test/java/com/bottega/tickets/concert/fixtures/ConcertOperations.java
@@ -1,8 +1,20 @@
 package com.bottega.tickets.concert.fixtures;
 
+import com.bottega.tickets.concert.SeleniumFixtures;
 import lombok.AllArgsConstructor;
+import org.openqa.selenium.By;
 
 @AllArgsConstructor
 public class ConcertOperations {
 
+    private SeleniumFixtures seleniumFixtures;
+
+    public static ConcertOperations init(SeleniumFixtures seleniumFixtures) {
+        return new ConcertOperations(seleniumFixtures);
+    }
+
+    public ConcertOperations openCreateConcertView() {
+        seleniumFixtures.driver.findElement(By.id("menu-concerts-add")).click();
+        return this;
+    }
 }
