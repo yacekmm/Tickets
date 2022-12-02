78c9d63b5f3d2b2b214a3e1d612eabbc4374a530
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-12-02 12:02:20 +0100

    comp-read-done

--- services/Vendor/src/test/java/com/bottega/vendor/concertRead/ConcertReadService_findConcerts_compTest.java
+++ services/Vendor/src/test/java/com/bottega/vendor/concertRead/ConcertReadService_findConcerts_compTest.java
@@ -1,7 +1,12 @@
 package com.bottega.vendor.concertRead;
 
+import java.util.List;
+
+import com.bottega.vendor.concert.domain.Concert;
 import com.bottega.vendor.concert.fixtures.ConcertLogicTestBase;
 import org.junit.jupiter.api.Test;
+import static com.bottega.sharedlib.config.TestClockConfig.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConcertReadService_findConcerts_compTest extends ConcertLogicTestBase {
 
@@ -9,11 +14,15 @@ public class ConcertReadService_findConcerts_compTest extends ConcertLogicTestBa
     @Test
     public void findConcerts_returnsSortedConcertsForVendor_onValidRequest() {
         //given
+        Concert concert_1 = builders.aConcert().withDate(TEST_TIME_PLUS_30_DAYS).withVendorId("vendor").inFinderDb();
+        Concert concert_2 = builders.aConcert().withDate(TEST_TIME_PLUS_60_DAYS).withVendorId("vendor").inFinderDb();
+        builders.aConcert().withVendorId("other").inDb();
 
         //when
+        List<Concert> result = concertReadFixtures.concertReadService.findConcertsForVendor("vendor");
 
         //then
-
+        assertThat(result).containsExactly(concert_1, concert_2);
     }
 
 }
