9ade8f97ecfedaf392fa84dff471b3a521eb0d31
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-12-02 12:55:56 +0100

    api-cdc-kafka-listener-start

--- services/Pricing/src/test/java/com/bottega/pricing/initialPrice/api/event/InitialPriceEventListener_settleInitialPrice_eventContractApiTest.java
+++ services/Pricing/src/test/java/com/bottega/pricing/initialPrice/api/event/InitialPriceEventListener_settleInitialPrice_eventContractApiTest.java
@@ -2,12 +2,9 @@ package com.bottega.pricing.initialPrice.api.event;
 
 import com.bottega.pricing.fixtures.FrameworkTestBase;
 import com.bottega.pricing.price.domain.ItemPrice;
-import com.bottega.pricing.price.fixtures.PriceAssert;
-import com.bottega.sharedlib.fixtures.EventAssert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.contract.stubrunner.StubTrigger;
-import static com.bottega.sharedlib.fixtures.RepoEntries.SINGULAR;
 import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
 
 class InitialPriceEventListener_settleInitialPrice_eventContractApiTest extends FrameworkTestBase {
@@ -28,13 +25,9 @@ class InitialPriceEventListener_settleInitialPrice_eventContractApiTest extends 
 
                     ItemPrice actualPrice = priceFixtures.itemPriceRepo.findAll().iterator().next();
 
-                    PriceAssert.assertThatPrice(actualPrice)
-                            .isPersistedIn(priceFixtures.itemPriceRepo, SINGULAR)
-                            .hasPrice(105_00)
-                            .hasNoFactors();
+                    //TODO: assert that price is updated with expected values and persisted in DB
 
-                    EventAssert.assertThatEventV1(sharedFixtures.testEventListener.singleEvent())
-                            .isPriceChange(actualPrice.getPrice().toInt(), actualPrice.getId().asString(), actualPrice.getItemId());
+                    //TODO: Assert that PRICE_CHANGED event was published, using TestEventListener
                 }
         );
     }
