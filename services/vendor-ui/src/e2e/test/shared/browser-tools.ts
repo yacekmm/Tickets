bb4d025f1f9af2df18216d62010f931b85ee1140
Jacek Milewski <jacek.milewski.k@gmail.com>
2022-12-02 12:59:16 +0100

    live-api-gui-start

--- services/vendor-ui/src/e2e/test/shared/browser-tools.ts
+++ services/vendor-ui/src/e2e/test/shared/browser-tools.ts
@@ -1,18 +1,3 @@
-import {browser, ElementFinder, protractor} from "protractor";
-
 export class BrowserTools {
 
-  initBrowser() {
-    browser.ignoreSynchronization = true;
-    browser.get('http://localhost:4200/');
-  }
-
-  getPageTitle() {
-    return browser.driver.getTitle();
-  }
-
-  waitUntilIsVisible(element: ElementFinder) {
-    let expected = protractor.ExpectedConditions;
-    return browser.wait(expected.visibilityOf(element));
-  }
 }
