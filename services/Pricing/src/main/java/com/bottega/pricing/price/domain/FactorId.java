c2fe9204b4a8cb5a993fb19f42ebaa86a5c8b17b
Jacek Milewski <jacek.milewski@circlekeurope.com>
2023-11-16 16:43:09 +0100

    Pricing upgrades

--- services/Pricing/src/main/java/com/bottega/pricing/price/domain/FactorId.java
+++ services/Pricing/src/main/java/com/bottega/pricing/price/domain/FactorId.java
@@ -1,8 +1,8 @@
 package com.bottega.pricing.price.domain;
 
 import com.bottega.sharedlib.repo.AggregateId;
+import jakarta.persistence.Embeddable;
 
-import javax.persistence.Embeddable;
 
 @Embeddable
 public class FactorId extends AggregateId {
--- services/Pricing/src/main/java/com/bottega/pricing/price/domain/PriceFactor.java
+++ services/Pricing/src/main/java/com/bottega/pricing/price/domain/PriceFactor.java
@@ -4,20 +4,14 @@ package com.bottega.pricing.price.domain;
 import com.bottega.sharedlib.ddd.DomainEntity;
 import com.bottega.sharedlib.repo.BaseEntity;
 import com.bottega.sharedlib.vo.Money;
-import com.vladmihalcea.hibernate.type.json.JsonStringType;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.vladmihalcea.hibernate.type.json.JsonType;
+import jakarta.persistence.*;
+import lombok.*;
 import org.hibernate.annotations.Type;
-import org.hibernate.annotations.TypeDef;
-import org.hibernate.annotations.TypeDefs;
-
-import javax.persistence.*;
 
 @DomainEntity
 @Entity
 @Table(name = "price_factors")
-@TypeDefs({@TypeDef(name = "jsonb", typeClass = JsonStringType.class)})
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
@@ -29,8 +23,8 @@ public class PriceFactor implements BaseEntity {
     @ManyToOne
     private ItemPrice price;
 
-    @Column(name = "policy")
-    @Type(type = "jsonb")
+    @Column(name = "policy", columnDefinition = "jsonb")
+    @Type(JsonType.class)
     FactorPolicy factorPolicy;
 
     //TODO/JM: unit test
