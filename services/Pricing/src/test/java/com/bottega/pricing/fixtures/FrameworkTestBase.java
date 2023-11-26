82c5bb39890179d9712adf01cda6ab2937982062
Jacek Milewski <jacek.milewski.k@gmail.com>
2023-11-26 20:15:18 +0100

    KafkaFixtures: fixed Listener exception on json format

--- services/Pricing/src/test/java/com/bottega/pricing/fixtures/FrameworkTestBase.java
+++ services/Pricing/src/test/java/com/bottega/pricing/fixtures/FrameworkTestBase.java
@@ -21,7 +21,6 @@ import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
 @ActiveProfiles({"test"})
 @AutoConfigureStubRunner(ids = CDC_STUB_ID_VENDOR, stubsMode = LOCAL)
 @Testcontainers
-//@AutoConfigureMessageVerifier
 @Slf4j
 public class FrameworkTestBase {
 
@@ -67,97 +66,4 @@ public class FrameworkTestBase {
         priceFixtures.tearDown();
         sharedFixtures.tearDown();
     }
-//    @Autowired
-//    private KafkaMessageVerifier kafkaMessageVerifier;
-
-//    void beforeEach() {
-//        kafkaMessageVerifier.reset();
-//    }
 }
-//@Configuration
-//@Slf4j
-//class TestConfig {
-//
-//    @Bean
-//    MessageVerifierSender<Message<?>> standaloneMessageVerifier(KafkaTemplate kafkaTemplate) {
-//        return new MessageVerifierSender<>() {
-//
-//            @Override
-//            public void send(Message<?> message, String destination, @Nullable YamlContract contract) {
-//            }
-//
-//            @Override
-//            public <T> void send(T payload, Map<String, Object> headers, String destination, @Nullable YamlContract contract) {
-//                Map<String, Object> newHeaders = headers != null ? new HashMap<>(headers) : new HashMap<>();
-//                newHeaders.put(KafkaHeaders.TOPIC, destination);
-//                log.info("Sending: " + payload + destination);
-//                kafkaTemplate.send(MessageBuilder.createMessage(payload, new MessageHeaders(newHeaders)));
-//            }
-//        };
-//    }
-//
-//    @Bean
-//    @Primary
-//    JsonMessageConverter noopMessageConverter() {
-//        return new NoopJsonMessageConverter();
-//    }
-//
-//    @Bean
-//    KafkaMessageVerifier kafkaTemplateMessageVerifier() {
-//        return new KafkaMessageVerifier();
-//    }
-//}
-//
-//
-//class KafkaMessageVerifier implements MessageVerifierReceiver<Message<?>> {
-//
-//    private static final Log LOG = LogFactory.getLog(KafkaMessageVerifier.class);
-//
-//    Map<String, BlockingQueue<Message<?>>> broker = new ConcurrentHashMap<>();
-//
-//
-//    @Override
-//    public Message receive(String destination, long timeout, TimeUnit timeUnit, @Nullable YamlContract contract) {
-//        broker.putIfAbsent(destination, new ArrayBlockingQueue<>(1));
-//        BlockingQueue<Message<?>> messageQueue = broker.get(destination);
-//        Message<?> message;
-//        try {
-//            message = messageQueue.poll(timeout, timeUnit);
-//        }
-//        catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
-//        if (message != null) {
-//            LOG.info("Removed a message from a topic [" + destination + "]");
-//        }
-//        return message;
-//    }
-//
-//
-//    @KafkaListener(id = "baristaContractTestListener", topicPattern = ".*")
-//    public void listen(ConsumerRecord payload, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
-//        LOG.info("Got a message from a topic [" + topic + "]");
-//        Map<String, Object> headers = new HashMap<>();
-//        new DefaultKafkaHeaderMapper().toHeaders(payload.headers(), headers);
-//        broker.putIfAbsent(topic, new ArrayBlockingQueue<>(1));
-//        BlockingQueue<Message<?>> messageQueue = broker.get(topic);
-//        messageQueue.add(MessageBuilder.createMessage(payload.value(), new MessageHeaders(headers)));
-//    }
-//
-//    @Override
-//    public Message receive(String destination, YamlContract contract) {
-//        return receive(destination, 15, TimeUnit.SECONDS, contract);
-//    }
-//
-//}
-
-//class NoopJsonMessageConverter extends JsonMessageConverter {
-//
-//    NoopJsonMessageConverter() {
-//    }
-//
-//    @Override
-//    protected Object convertPayload(Message<?> message) {
-//        return message.getPayload();
-//    }
-//}
