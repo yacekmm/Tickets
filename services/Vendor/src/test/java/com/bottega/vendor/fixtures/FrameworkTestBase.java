21a2b6ab0cc9bfd4f43ac480ccbd8b71839491f2
Jacek Milewski <jacek.milewski.k@gmail.com>
2023-11-26 20:46:03 +0100

    fixed vendor setup: kafka urls

--- services/Vendor/src/test/java/com/bottega/vendor/fixtures/FrameworkTestBase.java
+++ services/Vendor/src/test/java/com/bottega/vendor/fixtures/FrameworkTestBase.java
@@ -1,23 +1,28 @@
 package com.bottega.vendor.fixtures;
 
 import com.bottega.vendor.concert.domain.ConcertFixtures;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.groovy.util.Maps;
 import org.apache.kafka.clients.admin.AdminClient;
-import org.junit.jupiter.api.AfterEach;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.*;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 import static com.bottega.sharedlib.config.CdcStubs.CDC_STUB_ID_PRICING;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
 import static org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties.StubsMode.LOCAL;
+import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
 
 @SpringBootTest(webEnvironment = DEFINED_PORT)
 @ActiveProfiles({"test"})
 @AutoConfigureStubRunner(ids = CDC_STUB_ID_PRICING, stubsMode = LOCAL)
 @Testcontainers
+@Slf4j
 public class FrameworkTestBase {
 
 
@@ -40,6 +45,22 @@ public class FrameworkTestBase {
     protected SharedFixtures sharedFixtures;
     @Autowired
     protected KafkaContainerFixtures kafkaContainerFixtures;
+
+    @DynamicPropertySource
+    static void kafkaProperties(DynamicPropertyRegistry registry) {
+        await().until(kafka::isRunning);
+        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+
+        kafkaAdmin = AdminClient.create(Maps.of(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers()));
+
+        log.info("kafka configured at " + kafka.getBootstrapServers());
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        kafkaContainerFixtures.beforeEach();
+    }
 
 
     @AfterEach
