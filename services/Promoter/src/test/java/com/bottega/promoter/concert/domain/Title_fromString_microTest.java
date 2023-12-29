705a190a46f6ece5750bf2ab7ec0386f038c322a
Jacek Milewski <jacek.milewski.k@gmail.com>
2023-12-29 15:46:03 +0100

    task-assert-sample

--- services/Promoter/src/test/java/com/bottega/promoter/concert/domain/Title_fromString_microTest.java
+++ services/Promoter/src/test/java/com/bottega/promoter/concert/domain/Title_fromString_microTest.java
@@ -5,6 +5,7 @@ import org.assertj.vavr.api.VavrAssertions;
 import org.junit.jupiter.api.Test;
 import static com.bottega.promoter.concert.api.app.ConcertErrorCode.invalid_title;
 import static com.bottega.promoter.concert.domain.Title.from;
+import static com.bottega.promoter.concert.fixtures.TitleAssert.assertThatTitle;
 import static com.bottega.sharedlib.vo.error.ErrorResult.badRequest;
 import static org.apache.commons.lang3.StringUtils.repeat;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,7 +15,7 @@ class Title_fromString_microTest extends ConcertLogicTestBase {
     @Test
     public void fromString_OK_onValidString(){
         //expect
-        assertThat(from("This is a valid title for a concert").get().getValue()).isEqualTo("This is a valid title for a concert");
+        assertThatTitle(from("This is a valid title for a concert")).hasValue("This is a valid title for a concert");
     }
 
     @Test
@@ -23,7 +24,7 @@ class Title_fromString_microTest extends ConcertLogicTestBase {
         String expectedValue = repeat("a", 10);
 
         //expect
-        assertThat(from("    " + expectedValue + "     ").get().getValue()).isEqualTo(expectedValue);
+        assertThatTitle(from("     " + expectedValue + "    ")).hasValue(expectedValue);
     }
 
     @Test
