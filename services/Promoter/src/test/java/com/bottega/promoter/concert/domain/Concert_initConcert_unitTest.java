e18ddb210b81a01eadb547be4d543b46850dfd73
Jacek Milewski <jacek.milewski.k@gmail.com>
2023-12-29 17:12:35 +0100

    task-unit-sample

--- services/Promoter/src/test/java/com/bottega/promoter/concert/domain/Concert_initConcert_unitTest.java
+++ services/Promoter/src/test/java/com/bottega/promoter/concert/domain/Concert_initConcert_unitTest.java
@@ -1,27 +1,39 @@
 package com.bottega.promoter.concert.domain;
 
-import java.util.HashSet;
+import java.util.*;
 import java.util.stream.Stream;
 
 import com.bottega.promoter.concert.fixtures.ConcertLogicTestBase;
+import com.bottega.sharedlib.config.TestClockConfig;
 import org.junit.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
 import static com.bottega.sharedlib.config.TestClockConfig.TEST_TIME_PLUS_30_DAYS;
+import static java.util.stream.Collectors.toSet;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class Concert_initConcert_unitTest extends ConcertLogicTestBase {
 
-    @Test
-    void initConcert_addsTags() {
-        //TODO tests
+    private static Stream<Arguments> provideStringsForTags() {
+        return Stream.of(
+                Arguments.of("no tags apply", Set.of())
+                //TODO more cases
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideStringsForTags")
+    void initConcert_addsTags(String title, Set<String> expectedTags) {
         //given
+        Concert newConcert = new Concert(new ConcertId(), Title.from(title).get(), ConcertDate.from(TEST_TIME_PLUS_30_DAYS.toString(), sharedFixtures.clock).get(), "vendor-id", new HashSet<>(), null);
 
         //when
+        newConcert.initNewConcert(concertFixtures.tagService, concertFixtures.categoryService);
 
         //then
-
+        assertThat(newConcert.getTags().stream().map(Tag::getValue).collect(toSet())).containsExactlyInAnyOrderElementsOf(expectedTags);
     }
+
 
     private static Stream<Arguments> provideStringsForCategories() {
         return Stream.of(
@@ -40,7 +52,7 @@ class Concert_initConcert_unitTest extends ConcertLogicTestBase {
         Concert newConcert = new Concert(new ConcertId(), Title.from(title).get(), ConcertDate.from(TEST_TIME_PLUS_30_DAYS.toString(), sharedFixtures.clock).get(), "vendor-id", new HashSet<>(), null);
 
         //when
-        newConcert.initNewConcert(concertFixtures.categoryService);
+        newConcert.initNewConcert(concertFixtures.tagService, concertFixtures.categoryService);
 
         //then
         assertThat(newConcert.getCategory().getValue()).isEqualTo(expectedCategory);
