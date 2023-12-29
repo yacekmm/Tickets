74c7ea1fd0da52adf3942c863b8fc77578025ca9
Jacek Milewski <jacek.milewski.k@gmail.com>
2023-12-29 16:02:07 +0100

    task-spock-start

--- services/Promoter/src/test/java/com/bottega/promoter/concert/domain/Title_fromString_microSpec.groovy
+++ services/Promoter/src/test/java/com/bottega/promoter/concert/domain/Title_fromString_microSpec.groovy
@@ -8,50 +8,10 @@ import static com.bottega.promoter.concert.fixtures.TitleAssertSpock.assertThatT
 
 class Title_fromString_microSpec extends SpecificationBase {
 
+    //TODO tests
+
     def "fromString - OK - on valid input"() {
 
-        expect:
-        assertThatTitle(from(titleString))
-                .isEqualTo(expectedTitle)
-
-        where:
-        titleString                  | expectedTitle
-        "Valid title for a concert"  | "Valid title for a concert"
-        "     trims whitespaces    " | "trims whitespaces"
-
-        //accepts within length limits
-        "a".multiply(10)             | "a".multiply(10)
-        "a".multiply(160)            | "a".multiply(160)
-        "a".multiply(160) + " "      | "a".multiply(160)
     }
 
-    def "fromString - returns error - on invalid length"() {
-
-        expect:
-        assertThatTitle(from(titleString))
-                .hasInvalidLengthError()
-
-        where:
-        titleString << [
-                null,
-                "a" * 9,
-                "a" * 161,
-                "a" * 9 + " "]
-    }
-
-    def "fromString - returns error - on banned words in title"() {
-
-        expect:
-        assertThatTitle(from(titleString))
-                .hasBannedWordError(expectedBannedWord)
-
-        where:
-        titleString                | expectedBannedWord
-        "rage against the machine" | "rage"
-        "VIOLENCE"                 | "violence"
-        "   Snickers   "           | "snickers"
-        "rage against the machine" | "rage"
-        "machine against the rage" | "rage"
-        "machine rage machine"     | "rage"
-    }
 }
