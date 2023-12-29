acfe344d734046d83e0915cdd86d7bde3a1fd7e1
Jacek Milewski <jacek.milewski.k@gmail.com>
2023-12-29 11:44:04 +0100

    micro test tweak

--- services/Promoter/src/test/java/com/bottega/promoter/concert/domain/ConcertDate_fromString_microTest.java
+++ services/Promoter/src/test/java/com/bottega/promoter/concert/domain/ConcertDate_fromString_microTest.java
@@ -1,46 +1,68 @@
 package com.bottega.promoter.concert.domain;
 
-import com.bottega.promoter.concert.fixtures.*;
+import java.util.stream.Stream;
+
+import com.bottega.promoter.concert.fixtures.ConcertLogicTestBase;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
 import static com.bottega.promoter.concert.domain.ConcertDate.from;
+import static com.bottega.promoter.concert.fixtures.ConcertDateAssert.assertThatConcertDate;
 import static com.bottega.sharedlib.config.TestClockConfig.TEST_TIME;
 import static java.time.temporal.ChronoUnit.DAYS;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class ConcertDate_fromString_microTest extends ConcertLogicTestBase {
 
     @Test
     public void fromString_OK_onDateString(){
         //expect
-        ConcertDateAssert.assertThatConcertDate(from("2022-02-22", sharedFixtures.clock)).isEqualTo(2022, 2, 22);
-        ConcertDateAssert.assertThatConcertDate(from("2025-01-01", sharedFixtures.clock)).isEqualTo(2025, 1, 1);
-        ConcertDateAssert.assertThatConcertDate(from("2025-12-31", sharedFixtures.clock)).isEqualTo(2025, 12, 31);
+        assertThatConcertDate(from("2022-02-22", sharedFixtures.clock)).isEqualTo(2022, 2, 22);
+        assertThatConcertDate(from("2025-01-01", sharedFixtures.clock)).isEqualTo(2025, 1, 1);
+        assertThatConcertDate(from("2025-12-31", sharedFixtures.clock)).isEqualTo(2025, 12, 31);
     }
 
     @Test
     public void fromString_OK_onDateTimeString(){
         //expect
-        ConcertDateAssert.assertThatConcertDate(from("2022-02-27T07:20:00Z", sharedFixtures.clock)).isEqualTo(2022, 2, 27);
+        assertThatConcertDate(from("2022-02-27T07:20:00Z", sharedFixtures.clock)).isEqualTo(2022, 2, 27);
     }
 
     @Test
     public void fromString_badRequest_onInvalidString(){
         //expect
-        ConcertDateAssert.assertThatConcertDate(from("invalid", sharedFixtures.clock)).hasInvalidDateError("Unsupported date format: Text 'invalid' could not be parsed at index 0");
+        assertThatConcertDate(from("invalid", sharedFixtures.clock)).hasInvalidDateError("Unsupported date format: Text 'invalid' could not be parsed at index 0");
     }
 
     @Test
     public void fromString_error_onPastDate(){
         //expect
-        ConcertDateAssert.assertThatConcertDate(from(TEST_TIME.minus(1, DAYS).toString(), sharedFixtures.clock)).hasInvalidDateError("Too early");
-        ConcertDateAssert.assertThatConcertDate(from(TEST_TIME.minus(30, DAYS).toString(), sharedFixtures.clock)).hasInvalidDateError("Too early");
+        assertThatConcertDate(from(TEST_TIME.minus(1, DAYS).toString(), sharedFixtures.clock)).hasInvalidDateError("Too early");
+        assertThatConcertDate(from(TEST_TIME.minus(30, DAYS).toString(), sharedFixtures.clock)).hasInvalidDateError("Too early");
+    }
+
+    public static Stream<Arguments> tooEarlyDatesSource() {
+        return Stream.of(
+                arguments(TEST_TIME.minus(1, DAYS).toString()),
+                arguments(TEST_TIME.toString()),
+                arguments(TEST_TIME.plus(6, DAYS).toString()),
+                arguments(TEST_TIME.plus(7, DAYS).toString())
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("tooEarlyDatesSource")
+    public void fromString_returnsError_onDateUnderMinimumThreshold(String invalidDate){
+        //expect
+        assertThatConcertDate(from(TEST_TIME.plus(6, DAYS).toString(), sharedFixtures.clock)).hasInvalidDateError("Too early");
+        assertThatConcertDate(from(TEST_TIME.plus(7, DAYS).toString(), sharedFixtures.clock)).hasInvalidDateError("Too early");
+        assertThatConcertDate(from(TEST_TIME.plus(8, DAYS).toString(), sharedFixtures.clock)).isValid();
+        assertThatConcertDate(from(TEST_TIME.plus(9, DAYS).toString(), sharedFixtures.clock)).isValid();
     }
 
     @Test
-    public void fromString_error_onDateUnderMinimumThreshold(){
+    public void fromString_OK_onDateRightOverThreshold(){
         //expect
-        ConcertDateAssert.assertThatConcertDate(from(TEST_TIME.plus(6, DAYS).toString(), sharedFixtures.clock)).hasInvalidDateError("Too early");
-        ConcertDateAssert.assertThatConcertDate(from(TEST_TIME.plus(7, DAYS).toString(), sharedFixtures.clock)).hasInvalidDateError("Too early");
-        ConcertDateAssert.assertThatConcertDate(from(TEST_TIME.plus(8, DAYS).toString(), sharedFixtures.clock)).isValid();
-        ConcertDateAssert.assertThatConcertDate(from(TEST_TIME.plus(9, DAYS).toString(), sharedFixtures.clock)).isValid();
+        assertThatConcertDate(from(TEST_TIME.plus(8, DAYS).toString(), sharedFixtures.clock)).isValid();
     }
 }
