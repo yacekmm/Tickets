751c91cb8ac41837c1e1c35f64b0a053a6a98931
Jacek Milewski <jacek.milewski.k@gmail.com>
2023-12-29 17:37:33 +0100

    task-builder-start

--- services/Promoter/src/test/java/com/bottega/promoter/concert/domain/Concert_initConcert_unitTest.java
+++ services/Promoter/src/test/java/com/bottega/promoter/concert/domain/Concert_initConcert_unitTest.java
@@ -1,11 +1,12 @@
 package com.bottega.promoter.concert.domain;
 
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Stream;
 
 import com.bottega.promoter.concert.fixtures.ConcertLogicTestBase;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
+import static com.bottega.sharedlib.config.TestClockConfig.TEST_TIME_PLUS_30_DAYS;
 import static java.util.stream.Collectors.toSet;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,7 +27,8 @@ class Concert_initConcert_unitTest extends ConcertLogicTestBase {
     @MethodSource("provideStringsForTags")
     void initConcert_addsTags(String title, Set<String> expectedTags) {
         //given
-        Concert newConcert = builders.aConcert().withTitle(title).build();
+        //TODO use builder
+        Concert newConcert = new Concert(new ConcertId(), Title.from(title).get(), ConcertDate.from(TEST_TIME_PLUS_30_DAYS.toString(), sharedFixtures.clock).get(), "vendor-id", new HashSet<>(), null);
 
         //when
         newConcert.initNewConcert(concertFixtures.tagService, concertFixtures.categoryService);
@@ -50,7 +52,8 @@ class Concert_initConcert_unitTest extends ConcertLogicTestBase {
     @MethodSource("provideStringsForCategories")
     void initConcert_assignsCategory(String title, String expectedCategory) {
         //given
-        Concert newConcert = builders.aConcert().withTitle(title).build();
+        //TODO use builder
+        Concert newConcert = new Concert(new ConcertId(), Title.from(title).get(), ConcertDate.from(TEST_TIME_PLUS_30_DAYS.toString(), sharedFixtures.clock).get(), "vendor-id", new HashSet<>(), null);
 
         //when
         newConcert.initNewConcert(concertFixtures.tagService, concertFixtures.categoryService);
