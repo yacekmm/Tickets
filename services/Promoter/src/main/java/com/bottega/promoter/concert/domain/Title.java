81b5f6ab6d421fa393f3e658096f4c3080505f6f
Jacek Milewski <jacek.milewski.k@gmail.com>
2023-12-29 14:38:13 +0100

    task-micro-start-test-after

--- services/Promoter/src/main/java/com/bottega/promoter/concert/domain/Title.java
+++ services/Promoter/src/main/java/com/bottega/promoter/concert/domain/Title.java
@@ -31,8 +31,22 @@ public class Title {
     private String value;
 
     public static Validation<ErrorResult, Title> from(String title) {
-        //TODO implementation
-        return null;
+
+        String trimmed = defaultString(trim(title), "");
+
+        Optional<String> bannedWord = BANNED_WORDS.stream()
+                .filter(word -> containsIgnoreCase(trimmed, word))
+                .findFirst();
+
+        if (bannedWord.isPresent()) {
+            return Validation.invalid(ErrorResult.badRequest(ConcertErrorCode.invalid_title, "Title must not contain banned word: %s", bannedWord.get()));
+        }
+
+        if (length(trimmed) < TITLE_MIN_LEN || length(trimmed) > TITLE_MAX_LEN) {
+            return Validation.invalid(badRequest(ConcertErrorCode.invalid_title, "Title length must be between %s and %s chars", TITLE_MIN_LEN, TITLE_MAX_LEN));
+        }
+
+        return Validation.valid(new Title(trimmed));
 
     }
 }
