3b3f59799c65befa3b2910cb8476f6cb6d742206
Jacek Milewski <jacek.milewski.k@gmail.com>
2023-12-29 17:32:09 +0100

    live-builder-done

--- services/Pricing/src/test/java/com/bottega/pricing/price/domain/ItemPrice_applyFactor_unitTest.java
+++ services/Pricing/src/test/java/com/bottega/pricing/price/domain/ItemPrice_applyFactor_unitTest.java
@@ -1,12 +1,7 @@
 package com.bottega.pricing.price.domain;
 
-import java.util.ArrayList;
-
 import com.bottega.pricing.fixtures.LogicTestBase;
 import com.bottega.pricing.price.fixtures.PriceAssert;
-import com.bottega.sharedlib.fixtures.UUIDs;
-import com.bottega.sharedlib.repo.MoneyDbEntity;
-import com.bottega.sharedlib.vo.Money;
 import org.junit.jupiter.api.Test;
 import static com.bottega.pricing.price.fixtures.PriceAssert.assertThatPrice;
 
@@ -15,8 +10,7 @@ class ItemPrice_applyFactor_unitTest extends LogicTestBase {
     @Test
     void applyPercentageFactor_discountsPrice_onValidRequest() {
         //given
-        //TODO use builder
-        ItemPrice itemPrice = new ItemPrice(new PriceId(), MoneyDbEntity.from(new Money(200_00)), UUIDs.zeros(), new ArrayList<>());
+        ItemPrice itemPrice = builders.aPrice().build();
 
         //when
         ItemPrice result = itemPrice.applyFactor(PriceFactorFactory.percentageFactor(10, itemPrice));
