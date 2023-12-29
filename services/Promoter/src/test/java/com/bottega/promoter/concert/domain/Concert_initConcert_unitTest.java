72424eeda6e7b8c14283b6304801859fc8a79759
Jacek Milewski <jacek.milewski.k@gmail.com>
2023-12-29 16:23:24 +0100

    live-unit-start-tdd

--- services/Promoter/src/test/java/com/bottega/promoter/concert/domain/Concert_initConcert_unitTest.java
+++ services/Promoter/src/test/java/com/bottega/promoter/concert/domain/Concert_initConcert_unitTest.java
@@ -11,53 +11,5 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class Concert_initConcert_unitTest extends ConcertLogicTestBase {
 
-
-    private static Stream<Arguments> provideStringsForTags() {
-        return Stream.of(
-                Arguments.of("no tags apply", Set.of()),
-                Arguments.of("Rock concert", Set.of("rock")),
-                Arguments.of("Scorpions in Warsaw!", Set.of("rock")),
-                Arguments.of("Scorpions on Mystic Festival", Set.of("rock", "festival")),
-                Arguments.of("Rihanna the best of", Set.of("pop"))
-        );
-    }
-
-    @ParameterizedTest
-    @MethodSource("provideStringsForTags")
-    void initConcert_addsTags(String title, Set<String> expectedTags) {
-        //given
-        Concert newConcert = builders.aConcert().withTitle(title).build();
-
-        //when
-        newConcert.initNewConcert(concertFixtures.tagService, concertFixtures.categoryService);
-
-        //then
-        assertThat(newConcert.getTags().stream().map(Tag::getValue).collect(toSet())).containsExactlyInAnyOrderElementsOf(expectedTags);
-    }
-
-
-    private static Stream<Arguments> provideStringsForCategories() {
-        return Stream.of(
-                Arguments.of("no category", "other"),
-                Arguments.of("Rock concert", "rock"),
-                Arguments.of("Scorpions in Warsaw!", "rock"),
-                Arguments.of("Scorpions on Mystic Festival", "rock"),
-                Arguments.of("Rihanna the best of", "superstar")
-        );
-    }
-
-    @ParameterizedTest
-    @MethodSource("provideStringsForCategories")
-    void initConcert_assignsCategory(String title, String expectedCategory) {
-        //given
-        Concert newConcert = builders.aConcert().withTitle(title).build();
-
-        //when
-        newConcert.initNewConcert(concertFixtures.tagService, concertFixtures.categoryService);
-
-        //then
-        assertThat(newConcert.getCategory().getValue()).isEqualTo(expectedCategory);
-    }
-
-
+    //TODO tests
 }
