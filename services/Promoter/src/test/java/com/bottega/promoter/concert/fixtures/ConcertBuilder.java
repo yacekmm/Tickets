fc72b7307ccecf794726cf31fcf3420c9c7c90b0
Jacek Milewski <jacek.milewski.k@gmail.com>
2023-12-29 17:40:51 +0100

    task-builder-init

--- services/Promoter/src/test/java/com/bottega/promoter/concert/fixtures/ConcertBuilder.java
+++ services/Promoter/src/test/java/com/bottega/promoter/concert/fixtures/ConcertBuilder.java
@@ -12,5 +12,19 @@ import static com.bottega.sharedlib.config.TestClockConfig.TEST_TIME_PLUS_30_DAY
 @Component
 public class ConcertBuilder {
 
+    private final Clock clock;
+    private final Concert.ConcertBuilder builder;
+
+    public ConcertBuilder(Clock clock) {
+        this.clock = clock;
+        this.builder = Concert.builder()
+                .id(new ConcertId())
+                .title(Title.from("mock title of a concert").get())
+                .date(ConcertDate.from(TEST_TIME_PLUS_30_DAYS.toString(), this.clock).get())
+                .promoterId("mock-promoter-id")
+                .tags(new HashSet<>())
+                .category(null);
+    }
+
     //TODO implement builder
 }
