49d12d567970abab40ea76cfa6db06322b1b55b6
Jacek Milewski <jacek.milewski.k@gmail.com>
2023-12-30 15:49:13 +0100

    live-api-done

--- services/Pricing/src/test/java/com/bottega/pricing/price/api/rest/PriceRestController_applyPercentageFactor_restApiTest.java
+++ services/Pricing/src/test/java/com/bottega/pricing/price/api/rest/PriceRestController_applyPercentageFactor_restApiTest.java
@@ -12,22 +12,26 @@ public class PriceRestController_applyPercentageFactor_restApiTest extends Frame
 
     @Test
     public void applyFactor_OK_onValidRequest() {
-        //TODO implement
         //given
+        ItemPrice itemPrice = builders.aPrice().priceForItem(100_00, "item-id").inDb();
 
         //when
+        ValidatableResponse response = priceFixtures.pricingHttpClient.applyPercentageFactor("item-id", 20);
 
         //then
-
+        assertThatPrice(response)
+                .hasSinglePrice(itemPrice, 80_00)
+                .hasSinglePercentageFactor(20);
     }
 
     @Test
     public void applyFactor_returns404_onItemNotFound() {
-        //TODO implement
         //when
+        ValidatableResponse response = priceFixtures.pricingHttpClient.applyPercentageFactor("non-existing-item-id", 10);
 
         //then
-
+        assertThatError(response)
+                .isNotFound("No price entries found for requested item. itemId: non-existing-item-id");
     }
 
 }
