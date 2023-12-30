b98b04ff1236cebfb3ea46142a4f715afdad491b
Jacek Milewski <jacek.milewski.k@gmail.com>
2023-12-30 15:58:25 +0100

    task-api-start

--- libs/SharedLib/src/main/java/com/bottega/sharedlib/vo/error/ErrorExceptionMapper.java
+++ libs/SharedLib/src/main/java/com/bottega/sharedlib/vo/error/ErrorExceptionMapper.java
@@ -2,9 +2,10 @@ package com.bottega.sharedlib.vo.error;
 
 import com.bottega.sharedlib.dto.ErrorDto;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @ControllerAdvice
@@ -17,7 +18,8 @@ public class ErrorExceptionMapper {
         ErrorResult error = exception.getError();
         log.info("Mapping Error to HTTP code: {}, error: {}", error.getType().getHttpStatus(), error);
         return ResponseEntity
-                .status(error.getType().getHttpStatus())
+                //TODO should it return 500 always?
+                .status(INTERNAL_SERVER_ERROR)
                 .contentType(APPLICATION_JSON)
                 .body(ErrorDto.from(error));
     }
