560ec7b38ae5fae52ecd946ff4a51063078739b9
Jacek Milewski <jacek.milewski.k@gmail.com>
2023-12-30 14:52:41 +0100

    live-comp-comp-start

--- services/Pricing/src/test/java/com/bottega/pricing/fixtures/PriceFixtures.java
+++ services/Pricing/src/test/java/com/bottega/pricing/fixtures/PriceFixtures.java
@@ -39,7 +39,8 @@ public class PriceFixtures {
     }
 
     private static void initMocks(PriceFixtures priceFixtures) {
-        priceFixtures.priceUpdateService = mock(PriceUpdateService.class);
+        //TODO initialize
+        priceFixtures.priceUpdateService = null;
     }
 
     private static void initRepos(PriceFixtures priceFixtures) {
