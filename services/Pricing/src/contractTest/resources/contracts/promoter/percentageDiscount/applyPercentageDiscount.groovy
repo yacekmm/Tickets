81480e678ca79c460af87991bbae4b02221fd9d4
Jacek Milewski <jacek.milewski.k@gmail.com>
2024-01-02 17:13:17 +0100

    live-consumer-scc-start

--- services/Pricing/src/contractTest/resources/contracts/promoter/percentageDiscount/applyPercentageDiscount.groovy
+++ services/Pricing/src/contractTest/resources/contracts/promoter/percentageDiscount/applyPercentageDiscount.groovy
@@ -12,35 +12,13 @@ Contract.make {
 
     request {
 
-        method POST()
+        //TODO specify request
 
-        url '/api/v1/item/00000000-0000-0000-0000-000000000000/price-factor/percentage'
-
-        headers {
-            contentType applicationJson()
-            accept applicationJson()
-        }
-
-        body(
-                percentage: 10
-        )
     }
 
     response {
-        status 200
-        headers {
-            contentType applicationJson()
-        }
-        body(
-                [[
-                      priceId: regex(uuid()),
-                      price  : 90_00,
-                      factors: [[
-                                        type : "PERCENTAGE",
-                                        value: 10
-                                ]
-                      ]
-              ]]
-        )
+
+        //TODO specify response
+
     }
 }
