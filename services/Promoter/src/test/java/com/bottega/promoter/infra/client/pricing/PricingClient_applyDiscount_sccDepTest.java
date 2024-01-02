48989b0dd10bc80d07d53b7c169fba91bd0b5573
Jacek Milewski <jacek.milewski.k@gmail.com>
2024-01-02 17:16:15 +0100

    live-consumer-scc-done

--- services/Promoter/src/test/java/com/bottega/promoter/infra/client/pricing/PricingClient_applyDiscount_sccDepTest.java
+++ services/Promoter/src/test/java/com/bottega/promoter/infra/client/pricing/PricingClient_applyDiscount_sccDepTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.reactive.function.client.WebClient;
 import static com.bottega.sharedlib.config.CdcStubs.SCC_PRICING_PORT;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PricingClient_applyDiscount_sccDepTest extends SccFrameworkTestBase {
 
@@ -25,20 +26,20 @@ public class PricingClient_applyDiscount_sccDepTest extends SccFrameworkTestBase
     public void setUp() {
         fakePricingClient = new FakePricingClient();
         WebClient sccPricingWebClient = WebClientsConfig.buildPricingWebClient(servicesProperties.getPricing().host(), SCC_PRICING_PORT);
-        //TODO init real pricingHttpClient against SCC stubs
+        realPricingClient = new HttpPricingClient(sccPricingWebClient);
     }
 
     @Test
     public void fakeApplyDiscount_isValid() {
         //given
-        ConcertId concertId = new ConcertId();
+        ConcertId concertId = new ConcertId("00000000-0000-0000-0000-000000000000");
 
         //when
-        //TODO call real pricingHttpClient
+        Either<ErrorResult, List<Price>> realResult = realPricingClient.applyPercentageDiscount(concertId, 10);
         Either<ErrorResult, List<Price>> fakeResult = fakePricingClient.applyPercentageDiscount(concertId, 10);
 
         //then
-        //TODO verify if real and fake results are equal
+        assertThat(fakeResult.get()).containsExactlyInAnyOrderElementsOf(realResult.get());
     }
 
 }
