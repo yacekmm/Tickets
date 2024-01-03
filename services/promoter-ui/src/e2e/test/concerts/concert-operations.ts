6fcb556563ef14ac2f7aeb73aaab0dd4afd5ab94
Jacek Milewski <jacek.milewski.k@gmail.com>
2024-01-03 12:57:21 +0100

    live-html-gui-done

--- services/promoter-ui/src/e2e/test/concerts/concert-operations.ts
+++ services/promoter-ui/src/e2e/test/concerts/concert-operations.ts
@@ -1,4 +1,5 @@
 import {BrowserTools} from "../shared/browser-tools";
+import {By, element} from "protractor";
 
 export class ConcertOperations {
   private browserTools: BrowserTools;
@@ -7,4 +8,42 @@ export class ConcertOperations {
     this.browserTools = browserTools;
   }
 
+  assertPageIsOpened() {
+    this.browserTools.getPageTitle().then(pageTitle => expect(pageTitle).toEqual('Promoter Platform'));
+    return this;
+  }
+
+  openAddConcert() {
+    element(By.id("menu-concerts-add")).click()
+    return this;
+  }
+
+  createConcert() {
+    element(By.id("input-concert-title")).sendKeys("concert Title");
+    element(By.id("create-concert-submit")).click();
+    return this;
+  }
+
+  assertCreateConcertSuccess() {
+    let successConfirmation = element(By.className("mat-simple-snackbar"));
+    this.browserTools.waitUntilIsVisible(successConfirmation)
+      .then(unused =>
+        expect(successConfirmation.isDisplayed()).toBeTruthy()
+      );
+    return this;
+  }
+
+  openConcertsList() {
+    element(By.id("menu-concerts-list")).click();
+    return this;
+  }
+
+  assertAllConcertsListed(expectedConcertTitles: string[]) {
+    element.all(By.className('mat-row'))
+      .all(By.className('concert-title'))
+      .getText()
+      .then(value =>
+        expect(value).toEqual(expectedConcertTitles)
+      );
+  }
 }
