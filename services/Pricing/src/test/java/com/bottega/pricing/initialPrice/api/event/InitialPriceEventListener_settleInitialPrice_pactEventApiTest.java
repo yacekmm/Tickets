7e9d6a63dcb4ffe871f323d9f2c0a9535deb4446
Jacek Milewski <jacek.milewski.k@gmail.com>
2024-01-16 22:47:13 +0100

    pact-messaging-consumer

---
+++ services/Pricing/src/test/java/com/bottega/pricing/initialPrice/api/event/InitialPriceEventListener_settleInitialPrice_pactEventApiTest.java
@@ -0,0 +1,93 @@
+package com.bottega.pricing.initialPrice.api.event;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import au.com.dius.pact.consumer.MessagePactBuilder;
+import au.com.dius.pact.consumer.junit5.*;
+import au.com.dius.pact.core.model.PactSpecVersion;
+import au.com.dius.pact.core.model.annotations.Pact;
+import au.com.dius.pact.core.model.messaging.*;
+import com.bottega.pricing.fixtures.*;
+import com.bottega.pricing.price.domain.ItemPrice;
+import com.bottega.sharedlib.event.Event;
+import com.bottega.sharedlib.event.payload.ConcertCreatedEventPayload;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.vavr.control.Try;
+import lombok.SneakyThrows;
+import org.apache.kafka.common.serialization.*;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import static com.bottega.pricing.price.fixtures.PriceAssert.assertThatPrice;
+import static com.bottega.sharedlib.event.EventType.CONCERT_CREATED;
+import static com.bottega.sharedlib.fixtures.RepoEntries.SINGULAR;
+import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
+
+@ExtendWith(PactConsumerTestExt.class)
+@PactTestFor(providerName = "Tickets.Promoter", providerType = ProviderType.ASYNCH, pactVersion = PactSpecVersion.V3)
+class InitialPriceEventListener_settleInitialPrice_pactEventApiTest extends FrameworkTestBase {
+
+    @Autowired
+    ObjectMapper objectMapper;
+    StringSerializer stringSerializer = new StringSerializer();
+    StringDeserializer stringDeserializer = new StringDeserializer();
+
+    @BeforeEach
+    void setUp() {
+        super.beforeEach();
+        System.setProperty("pact.rootDir", "build/pacts");
+    }
+
+    @Pact(consumer = "Tickets.Pricing")
+    @SneakyThrows
+    MessagePact concertCreatedPact(MessagePactBuilder builder) {
+
+        Event event = Event.builder()
+                .type(CONCERT_CREATED)
+                .payload(new ConcertCreatedEventPayload(
+                        "id",
+                        "this has to be a valid title",
+                        "2025-12-12",
+                        new String[]{},
+                        5))
+                .build();
+
+        return builder.expectsToReceive("CONCERT_CREATED event")
+                .withContent(new String(stringSerializer.serialize("promoter.concert", objectMapper.writeValueAsString(event)), StandardCharsets.UTF_8))
+                .toPact();
+    }
+
+    @Test
+    @SneakyThrows
+    @PactTestFor(pactMethod = "concertCreatedPact", providerType = ProviderType.ASYNCH)
+    public void settleInitialPrice_createsPrice_OnContractTest(List<Message> messages) {
+        //when
+        triggerEvents(messages);
+
+        //then
+        await().until(() -> priceFixtures.itemPriceRepo.findAll().iterator().hasNext());
+
+        ItemPrice actualPrice = priceFixtures.itemPriceRepo.findAll().iterator().next();
+
+        assertThatPrice(actualPrice)
+                .isPersistedIn(priceFixtures.itemPriceRepo, SINGULAR)
+                .hasPrice(105_00)
+                .hasNoFactors();
+
+        PriceChangeEventAssert.assertThatEvent(sharedFixtures.testEventListener.singleEvent())
+                .isPriceChangeV1(actualPrice);
+    }
+
+    @SneakyThrows
+    private void triggerEvents(List<Message> messages) {
+        messages.stream()
+                .map(Message::contentsAsBytes)
+                .map(bytes -> stringDeserializer.deserialize("promoter.concert", bytes))
+                .map(deserialized -> Try.of(() -> objectMapper.readValue(deserialized, Event.class)))
+                .map(Try::get)
+                .forEach(sharedFixtures.eventPublisher::publish);
+    }
+
+
+}
