731e0045351872e54be3b217bf2156ff7c079c5d
Jacek Milewski <jacek.milewski.k@gmail.com>
2024-04-22 09:39:08 +0200

    ci-shared-lib

--- services/Pricing/src/test/java/com/bottega/pricing/price/api/rest/Pricing_providesApiContract_pactApiTest.java
+++ services/Pricing/src/test/java/com/bottega/pricing/price/api/rest/Pricing_providesApiContract_pactApiTest.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.info.GitProperties;
 
 @Provider("Tickets.Pricing")
-@PactBroker(url = "${PACT_BROKER_BASE_URL}", authentication = @PactBrokerAuth(token = "${PACT_BROKER_TOKEN}"))
+@PactBroker(url = "https://jacek.pactflow.io", authentication = @PactBrokerAuth(token = "${PACT_BROKER_TOKEN}"))
 public class Pricing_providesApiContract_pactApiTest extends FrameworkTestBase {
 
     @Value("${server.port}") int port;
