8932bf4fca514d012abb9c98da3012f6efcaa659
Jacek Milewski <jacek.milewski.k@gmail.com>
2024-05-04 15:44:27 +0200

    rebaser

--- .github/rebaser.sh
+++ .github/rebaser.sh
@@ -5,8 +5,26 @@
 
 
 
-git reset --hard HEAD
-git fetch origin
-git checkout -b test_branch origin/test_branch
-git rebase origin/main
-git push origin test_branch --force
+
+#git reset --hard HEAD
+#git fetch origin
+#git checkout -b test_branch origin/test_branch
+#git rebase origin/main
+#git push origin test_branch --force
+#git checkout -b main origin/main
+
+for branch in `git branch -r | grep -v HEAD | grep 'test_b' | cut -d/ -f2-`;do
+  branch_name=$(echo -e `git show --format="%ci" $branch | head -n 1` \\t$branch)
+
+  echo $branch_name
+
+  git reset --hard HEAD
+  git fetch origin
+  git checkout -b $branch_name origin/$branch_name
+  git rebase origin/main
+  git push origin $branch_name --force
+  git checkout -b main origin/main
+
+done | sort -r
+
+read
