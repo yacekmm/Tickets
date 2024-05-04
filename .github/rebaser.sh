ea98f46fed4267d2d96115700ad07b469bc6b747
Jacek Milewski <jacek.milewski.k@gmail.com>
2024-05-04 15:26:41 +0200

    test commit

---
+++ .github/rebaser.sh
@@ -0,0 +1,10 @@
+
+cd Tickets
+git config --global user.email "jacek.milewski.k@gmail.com"
+git config --global user.name "Jacek Milewski"
+
+git reset --hard HEAD
+git fetch origin
+git checkout -b test_branch origin/test_branch
+git rebase origin/main
+git push origin test_branch --force
