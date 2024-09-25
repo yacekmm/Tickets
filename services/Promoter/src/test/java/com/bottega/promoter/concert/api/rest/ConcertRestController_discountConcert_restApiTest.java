5a3f2095e92f2891702b076b996c9b71013a9492
Jacek Milewski <jacek.milewski.k@gmail.com>
2024-09-25 15:47:58 +0200

    price json assert

--- services/Promoter/src/test/java/com/bottega/promoter/concert/api/rest/ConcertRestController_discountConcert_restApiTest.java
+++ services/Promoter/src/test/java/com/bottega/promoter/concert/api/rest/ConcertRestController_discountConcert_restApiTest.java
@@ -1,12 +1,11 @@
 package com.bottega.promoter.concert.api.rest;
 
-import au.com.dius.pact.consumer.junit5.*;
 import com.bottega.promoter.concert.domain.Concert;
-import com.bottega.promoter.fixtures.*;
+import com.bottega.promoter.fixtures.PactFrameworkTestBase;
 import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.Test;
-import static org.apache.http.HttpStatus.SC_OK;
-import static org.hamcrest.Matchers.*;
+
+import static com.bottega.promoter.concert.api.rest.PriceJsonAssert.assertThatPrice;
 
 public class ConcertRestController_discountConcert_restApiTest extends PactFrameworkTestBase {
 
@@ -18,10 +17,8 @@ public class ConcertRestController_discountConcert_restApiTest extends PactFrame
         ValidatableResponse response = concertFixtures.concertHttpClient.discountConcert(concert.getId(), 10);
 
         //then
-        response
-                .statusCode(SC_OK)
-                .body("$", hasSize(1))
-                .body("[0].price", equalTo(90_00));
+        assertThatPrice(response)
+                .hasSinglePrice(90_00);
     }
 
 }
