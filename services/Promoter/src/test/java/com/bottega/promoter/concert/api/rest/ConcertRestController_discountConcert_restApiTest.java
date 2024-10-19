18f135025a7cf612f21999c4def0f49d2b0c26ef
Jacek Milewski <jacek.milewski.k@gmail.com>
2024-10-19 15:06:21 +0200

    task-consumer-pact-2-start

--- services/Promoter/src/test/java/com/bottega/promoter/concert/api/rest/ConcertRestController_discountConcert_restApiTest.java
+++ services/Promoter/src/test/java/com/bottega/promoter/concert/api/rest/ConcertRestController_discountConcert_restApiTest.java
@@ -1,15 +1,17 @@
 package com.bottega.promoter.concert.api.rest;
 
 import com.bottega.promoter.concert.domain.Concert;
-import com.bottega.promoter.fixtures.PactFrameworkTestBase;
+import com.bottega.promoter.fixtures.FrameworkTestBase;
 import io.restassured.response.ValidatableResponse;
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 
 import static com.bottega.promoter.concert.fixtures.PriceJsonAssert.assertThatPrice;
 
-public class ConcertRestController_discountConcert_restApiTest extends PactFrameworkTestBase {
+public class ConcertRestController_discountConcert_restApiTest extends FrameworkTestBase {
 
     @Test
+    @Ignore("Because CI build was red")
     public void discountConcert_discounts_onValidRequest() {
         //given
         Concert concert = builders.aConcert().withId("123").inDb();
--- services/Promoter/src/test/java/com/bottega/promoter/infra/client/pricing/PricingClient_applyDiscount_pactDepTest.java
+++ services/Promoter/src/test/java/com/bottega/promoter/infra/client/pricing/PricingClient_applyDiscount_pactDepTest.java
@@ -1,37 +1,20 @@
 package com.bottega.promoter.infra.client.pricing;
 
-import java.util.List;
+import com.bottega.promoter.fixtures.FrameworkTestBase;
+import org.junit.jupiter.api.Test;
 
-import com.bottega.promoter.concert.Price;
-import com.bottega.promoter.concert.domain.ConcertId;
-import com.bottega.promoter.fixtures.*;
-import com.bottega.sharedlib.vo.error.ErrorResult;
-import io.vavr.control.Either;
-import org.junit.jupiter.api.*;
-import static org.assertj.core.api.Assertions.assertThat;
-
-public class PricingClient_applyDiscount_pactDepTest extends PactFrameworkTestBase {
-
-    FakePricingClient fakePricingClient;
-    PricingClient realPricingClient;
-
-    @BeforeEach
-    void setUp() {
-        fakePricingClient = new FakePricingClient();
-        realPricingClient = concertFixtures.pricingClient;
-    }
+public class PricingClient_applyDiscount_pactDepTest extends FrameworkTestBase {
 
     @Test
-    public void fakeApplyDiscount_isValid() {
+    public void applyDiscount_isValid() {
         //given
-        ConcertId concertId = new ConcertId("123");
+
 
         //when
-        Either<ErrorResult, List<Price>> realResult = realPricingClient.applyPercentageDiscount(concertId, 10);
-        Either<ErrorResult, List<Price>> fakeResult = fakePricingClient.applyPercentageDiscount(concertId, 10);
+
 
         //then
-        assertThat(fakeResult.get()).containsExactlyInAnyOrderElementsOf(realResult.get());
+
     }
 
 }
