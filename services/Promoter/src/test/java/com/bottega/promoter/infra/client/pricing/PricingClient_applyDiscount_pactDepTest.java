8194e1a3191040e95f5374bbc5093c753d91fe1e
Jacek Milewski <jacek.milewski.k@gmail.com>
2024-10-19 15:13:39 +0200

    task-consumer-pact-2-failing-test

--- services/Promoter/src/test/java/com/bottega/promoter/infra/client/pricing/PricingClient_applyDiscount_pactDepTest.java
+++ services/Promoter/src/test/java/com/bottega/promoter/infra/client/pricing/PricingClient_applyDiscount_pactDepTest.java
@@ -1,20 +1,30 @@
 package com.bottega.promoter.infra.client.pricing;
 
+import com.bottega.promoter.concert.Price;
+import com.bottega.promoter.concert.domain.ConcertId;
 import com.bottega.promoter.fixtures.FrameworkTestBase;
+import com.bottega.sharedlib.vo.Money;
+import com.bottega.sharedlib.vo.error.ErrorResult;
+import io.vavr.control.Either;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static com.bottega.promoter.concert.fixtures.asserts.PriceAssert.assertThatPrice;
 
 public class PricingClient_applyDiscount_pactDepTest extends FrameworkTestBase {
 
     @Test
     public void applyDiscount_isValid() {
         //given
-
+        ConcertId concertId = new ConcertId("123");
 
         //when
-
+        Either<ErrorResult, List<Price>> result = concertFixtures.pricingClient.applyPercentageDiscount(concertId, 10);
 
         //then
-
+        assertThatPrice(result.get().get(0))
+                .equalTo(new Money(90_00));
     }
 
 }
