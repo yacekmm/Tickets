461476ce868aa075e0b033e47533fe0c9eb00236
Jacek Milewski <jacek.milewski.k@gmail.com>
2025-05-12 20:31:39 +0200

    Patterns in progress

---
+++ services/Payment/src/main/java/com/bottega/payment/infra/naiveasync/NaiveAsyncNotifier.java
@@ -0,0 +1,17 @@
+package com.bottega.payment.infra.naiveasync;
+
+import com.bottega.payment.domain.Payment;
+import com.bottega.payment.domain.ports.out.Notifier;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@RequiredArgsConstructor
+public class NaiveAsyncNotifier implements Notifier {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    @Override
+    public void sendConfirmation(Payment payment) {
+        kafkaTemplate.send("payment.completed", payment);
+    }
+}
--- services/Promoter/src/test/java/com/bottega/promoter/infra/client/pricing/PricingClient_applyDiscount_pactDepTest.java
+++ services/Promoter/src/test/java/com/bottega/promoter/infra/client/pricing/PricingClient_applyDiscount_pactDepTest.java
@@ -1,24 +1,27 @@
 package com.bottega.promoter.infra.client.pricing;
 
-import java.util.List;
-
 import com.bottega.promoter.concert.Price;
 import com.bottega.promoter.concert.domain.ConcertId;
-import com.bottega.promoter.fixtures.*;
+import com.bottega.promoter.fixtures.FakePricingClient;
+import com.bottega.promoter.fixtures.PactFrameworkTestBase;
 import com.bottega.sharedlib.vo.error.ErrorResult;
 import io.vavr.control.Either;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PricingClient_applyDiscount_pactDepTest extends PactFrameworkTestBase {
 
     FakePricingClient fakePricingClient;
-    PricingClient realPricingClient;
+    PricingClient pricingHttpClient;
 
     @BeforeEach
     void setUp() {
         fakePricingClient = new FakePricingClient();
-        realPricingClient = concertFixtures.pricingClient;
+        pricingHttpClient = concertFixtures.pricingClient;
     }
 
     @Test
@@ -27,7 +30,7 @@ public class PricingClient_applyDiscount_pactDepTest extends PactFrameworkTestBa
         ConcertId concertId = new ConcertId("123");
 
         //when
-        Either<ErrorResult, List<Price>> realResult = realPricingClient.applyPercentageDiscount(concertId, 10);
+        Price result = pricingHttpClient.applyPercentageDiscount(itemId, 10);
         Either<ErrorResult, List<Price>> fakeResult = fakePricingClient.applyPercentageDiscount(concertId, 10);
 
         //then
